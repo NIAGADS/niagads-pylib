aeb06c70baa41ff61d0eff96d99c7bbea1edc936
fossilfriend <egreenfest@gmail.com>
2023-09-14 12:39:46 -0400

    standardized logging

--- niagads/scripts/variant_annotator.py
+++ niagads/scripts/variant_annotator.py
@@ -7,7 +7,7 @@ import argparse
 import logging
 from sys import stdout
 
-from ..utils.sys import warning
+from ..utils.sys import warning, ExitOnExceptionHandler
 from ..api_wrapper import VariantRecord
 
 REQUEST_URI = "https://api.niagads.org"
@@ -31,6 +31,8 @@ def read_variants(file):
 
 
 def main():
+    logger = logging.getLogger(__name__)
+    
     parser = argparse.ArgumentParser(description="Look up a list of variants and retrieve annotation", allow_abbrev=False)
     parser.add_argument('--file', required=True,
                         help="new line separated list of variants, can be refSnpID or chr:pos:ref:alt")
@@ -45,20 +47,25 @@ def main():
     parser.add_argument('--debug', help="log debugging statements", action='store_true')
     args = parser.parse_args()
     
-
-    logging.basicConfig(filename=args.file + '-variant-annotator.log',
-                        filemode='w',
-                        encoding='utf-8',
-                        level=logging.DEBUG if args.debug else logging.INFO)
+    logging.basicConfig( handlers=[ExitOnExceptionHandler(
+                filename=args.file + '-variant-annotator.log',
+                mode='w',
+                encoding='utf-8',
+            )],
+            format='%(asctime)s %(levelname)-8s %(message)s',
+            level=logging.DEBUG if args.debug else logging.INFO)
     
-    variants = VariantRecord('genomics', REQUEST_URI, read_variants(args.file))
-    variants.set_null_str(args.nullStr)
-    variants.set_page_size(args.pageSize)
-    if args.allConseqs: variants.retrieve_full_annotation()
+    try:
+        variants = VariantRecord('genomics', REQUEST_URI, read_variants(args.file))
+        variants.set_null_str(args.nullStr)
+        variants.set_page_size(args.pageSize)
+        if args.allConseqs: variants.retrieve_full_annotation()
 
-    warning("Looking up ", variants.get_query_size(), "variants", prefix="INFO")
-    
-    variants.fetch()
-    variants.write_response(file=stdout, format=args.format)
-
+        logger.info("Looking up " + str(variants.get_query_size()) + " variants")
+        
+        variants.fetch()
+        variants.write_response(file=stdout, format=args.format)
+    except Exception as err:
+        logger.error("Error annotating variants", err, stack_info=True, exc_info=True)
+        raise err
     
