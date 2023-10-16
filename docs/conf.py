333b8a29ca2a819d3d02868787935f9574d18ad5
fossilfriend <egreenfest@gmail.com>
2023-10-16 11:29:01 -0400

    initial sphinx conf

---
+++ docs/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+from datetime import date
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+
+project = 'NIAGADS-pylib'
+copyright = copyright = f'{date.today().year}, University of Pennsylvania'
+author = 'NIAGADS'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+rst_epilog = f'''
+.. |project| replace:: {project}
+'''
+
+extensions = []
+
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+
