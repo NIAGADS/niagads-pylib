b2667fe4d542978d5f973fbb51c3bce46d4e44e8
fossilfriend <egreenfest@gmail.com>
2023-10-16 11:39:46 -0400

    index

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,7 @@ extensions = [
 # templates_path = ['_templates']
 exclude_patterns = ['_build']
 
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
