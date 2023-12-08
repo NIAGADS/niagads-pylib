7b8c35c13dedf7ad800a48f2a0359cc4de653aaf
fossilfriend <egreenfest@gmail.com>
2023-12-08 13:52:55 -0500

    format checkers

--- niagads/validators/json/format_checkers.py
+++ niagads/validators/json/format_checkers.py
@@ -1,16 +1,30 @@
-from jsonschema import FormatChecker
+''' Format Checkers are validators that assert 
+that the value corresponds to an expected format 
+built-in to Draft 7 are the following: https://json-schema.org/draft-07/json-schema-release-notes#formats
+
+below are additional format checkers required by NIAGADS projects
+
 # see https://lat.sk/2017/03/custom-json-schema-type-validator-format-python/
+for more information
+'''
 
-FORMAT_CHECKER = FormatChecker()
+from jsonschema import FormatChecker
+from ...utils.string import matches
+from ...utils import RegularExpressions as RE
 
-@FORMAT_CHECKER.checks('even', AssertionError)
-def even_number(value):
-    assert value % 2 == 0
+JSONSchemaFormatChecker = FormatChecker()
+
+@JSONSchemaFormatChecker.checks('pubmed_id', AssertionError)
+def pubmed_id(value):
+   assert matches(RE.PUBMED_ID, str(value))
+   return True
+
+@JSONSchemaFormatChecker.checks('doi', AssertionError)
+def doi(value):
+    assert matches(RE.DOI, str(value))
     return True
 
-@FORMAT_CHECKER.checks('pubmed_id', AssertionError)
-# TODO: handle both numeric and string check
-def pubmed_id(value):
-    if isinstance(value, int):
-        return True
-
+@JSONSchemaFormatChecker.checks('md5sum', AssertionError)
+def md5sum(value):
+    assert matches(RE.MD5SUM, str(value))
+    return True
