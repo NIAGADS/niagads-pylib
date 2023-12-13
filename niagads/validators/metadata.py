f3f2c5478de51af4710e8f84258e45c06973142c
fossilfriend <egreenfest@gmail.com>
2023-12-13 15:14:01 -0500

    abstract out

--- niagads/validators/metadata.py
+++ niagads/validators/metadata.py
@@ -1,5 +1,8 @@
 import logging
 from json import dumps as j_dumps
+from abc import ABC, abstractmethod 
+
+from typing import List
 
 from jsonschema import exceptions as jsExceptions
 
@@ -8,23 +11,32 @@ from ..parsers import ExcelFileParser, CSVFileParser
 from ..utils.sys import is_xlsx
 from ..utils.string import xstr
 
-class MetadataValidator:
+class CSVValidator(ABC):
     """
-    validate 2-column CSV format metadata file, with field names in
-    first column:
-    
-    field   value
-    
+    Abstract Base Class for JSON-schema based CSV validation
     """
     def __init__(self, fileName:str, schema, debug:bool=False):
         self._debug = debug
         self.logger  = logging.getLogger(__name__)
-        self.__file = fileName
-        self.__schema = schema
-        self.__metadata = None
-
+        self._file = fileName
+        self._schema = schema
+        self._metadata = None
     
-    def get_metadata_object(self, asString=False):
+    
+    def set_schema(self, schema):
+        """
+        set schema
+
+        Args:
+            schema (str|obj): schema file name or object
+        """
+        self._schema = schema
+        
+        
+    def get_schema(self):
+        return self._schema
+    
+    def get_metadata(self, asString=False):
         """
         retrieve the parsed metadata
 
@@ -35,47 +47,56 @@ class MetadataValidator:
             metadata as JSON or JSON str (`asString = True`)
             
         Raises:
-            ValueError is metadata has not yet been loaded
+            ValueError if metadata has not yet been loaded
         """
-        if self.__metadata is None:
+        if self._metadata is None:
             raise ValueError("metadata object has not yet been loaded; please run `load` before accessing")
-        return j_dumps(self.__metadata) if asString else self.__metadata
+        return j_dumps(self._metadata) if asString else self._metadata
+    
+    @abstractmethod
+    def load(self, worksheet:str=None):
+        raise NotImplementedError("`load` method has not been implement for the child of this Abstract parent class")
+    
+    @abstractmethod
+    def run(self, failOnError:bool=False):
+        raise NotImplementedError("`run` method has not been implement for the child of this Abstract parent class")
     
     
+class MetadataValidator(CSVValidator):
+    """
+    validate 2-column CSV format metadata file using a JSON-schema,
+    with field names in first column as follows:
+    
+    field   value
+    
+    no file header
+    """
+    def __init__(self, fileName:str, schema, debug:bool=False):
+        super().__init__(fileName, schema, debug)
+
     def load(self, worksheet:str=None):
         """
         load the metadata from a file, if EXCEL file specify name of worksheet to be extracted
         and convert to JSON
         
         worksheet (str|int, optional): worksheet name or index, starting from 0. Required for EXCEL files only.
-     
         Args:
             worksheet (str, optional): name of worksheet in EXCEL file to be validated. Defaults to None.
         """
-        if is_xlsx(self.__file):
-            parser = ExcelFileParser(self.__file)
+        if is_xlsx(self._file):
+            parser = ExcelFileParser(self._file)
             parser.strip()
             if worksheet is None:
                 raise TypeError("Metadata file is of type `xlsx` (EXCEL); must supply name of the worksheet to load")
-            self.__metadata = parser.worksheet_to_json(
+            self._metadata = parser.worksheet_to_json(
                 worksheet, transpose=True, returnStr=False, header=None, index_col=0)[0]            
 
         else:
-            parser = CSVFileParser(self.__file)
+            parser = CSVFileParser(self._file)
             parser.strip()
-            self.__metadata = parser.to_json(transpose=True, returnStr=False, header=None, index_col=0)[0]
+            self._metadata = parser.to_json(transpose=True, returnStr=False, header=None, index_col=0)[0]
     
-
-    def set_schema(self, schema):
-        """
-        set schema
-
-        Args:
-            schema (str|obj): schema file name or object
-        """
-        self.__schema = schema
-
-
+    
     def run(self, failOnError:bool=False):
         """
         run validation
@@ -90,66 +111,41 @@ class MetadataValidator:
         Raises:
             jsonschema.exceptions.ValidationError if `failOnError` = True
         """
-        validator = JSONValidator(self.__metadata, self.__schema, self._debug)
+        validator = JSONValidator(self._metadata, self._schema, self._debug)
         return validator.run(failOnError)
     
         
-class FileManifestValidator:
+class TableValidator(CSVValidator):
     """
-    validate a file manifest in CSV format, with column names
-    and 1 row per file
-    
-    also compares against list of samples / biosources to make sure
-    all biosources are known
+    validate metadata organized in CSV format, with column names
+    and 1 row per entity (e.g., file or biosource properties) 
+    to be validated
+
     """
     def __init__(self, fileName, schema, debug:bool=False):
-        self._debug = debug
-        self.logger  = logging.getLogger(__name__)
-        self.__schema = schema
-        self.__file = fileName
-        self.__metadata = None
+        super().__init__(fileName, schema, debug)
         
-    
-    def get_metadata_object(self, asString=False):
-        """
-        retrieve the parsed metadata
-
-        Args:
-            asString (bool, optional): _description_. Defaults to False.
-
-        Returns:
-            metadata as JSON or JSON str (`asString = True`)
-            
-        Raises:
-            ValueError is metadata has not yet been loaded
-        """
-        if self.__metadata is None:
-            raise ValueError("metadata object has not yet been loaded; please run `load` before accessing")
-        return j_dumps(self.__metadata) if asString else self.__metadata
-    
-
     def load(self, worksheet:str=None):
         """
         load the metadata from a file, if EXCEL file specify name of worksheet to be extracted
         and convert to JSON
         
         worksheet (str|int, optional): worksheet name or index, starting from 0. Required for EXCEL files only.
-     
+
         Args:
             worksheet (str, optional): name of worksheet in EXCEL file to be validated. Defaults to None.
         """
-        if is_xlsx(self.__file):
-            parser = ExcelFileParser(self.__file)
+        if is_xlsx(self._file):
+            parser = ExcelFileParser(self._file)
             if worksheet is None:
                 raise TypeError("Metadata file is of type `xlsx` (EXCEL); must supply name of the worksheet to load")
-            self.__metadata = parser.worksheet_to_json(
-                worksheet, returnStr=False, header=0)
+            self._metadata = parser.worksheet_to_json(worksheet, returnStr=False, header=0)
 
         else:
-            parser = CSVFileParser(self.__file)
-            self.__metadata = parser.to_json(returnStr=False, header=0)
-
-
+            parser = CSVFileParser(self._file)
+            self._metadata = parser.to_json(returnStr=False, header=0)
+            
+            
     def run(self, failOnError:bool=False):
         """
         run validation on each row
@@ -165,26 +161,49 @@ class FileManifestValidator:
         """
         
         result = []
-        validator = JSONValidator(None, self.__schema, self._debug)
-        for index, row in enumerate(self.__metadata):
+        validator = JSONValidator(None, self._schema, self._debug)
+        for index, row in enumerate(self._metadata):
             validator.set_json(row)
-            rowIsValid = validator.run()
-            if not failOnError:
-                result.append({index: rowIsValid})
-            elif not bool(rowIsValid):
-                validator.validation_error(rowIsValid, prefix="row " + xstr(index) + " - " + xstr(row))
+            rowValidation = validator.run()
+            if not isinstance(rowValidation, bool):
+                if failOnError:
+                    validator.validation_error(rowValidation, prefix="row " + xstr(index) + " - " + xstr(row))
+                else:
+                    result.append({index: rowValidation})
+        
+        return True if len(result) == 0 else result # empty array; all rows passed
+        
+        
+class FileManifestValidator(TableValidator):
+    """
+    validate a file manifest in CSV format, with column names
+    and 1 row per file
+    
+    also compares against list of samples / biosources to make sure
+    all biosources are known
+    """
+    def __init__(self, fileName, schema, debug:bool=False):
+        super().__init__(fileName, schema, debug)
+        
+        
+    def validate_samples(self, sampleField: str, sampleReference:List[str]):
+        """
+        verifies that samples in the file manifest are 
+        present in a reference list of samples
 
-        return result
+        Args:
+            sampleField (str): field in the sample manifest that contains sample IDs
+            sampleReference (List[str]): reference list of samples to match against
+        """
+        raise NotImplementedError("validating samples IDS in the file manifest coming soon")
+    
 
-class BiosourcePropertiesValidator:
+class BiosourcePropertiesValidator(TableValidator):
     """
     validate biosource properties in a CSV format file, with column names
     and 1 row per sample or subject
     
     """
     def __init__(self, fileName, schema, debug:bool=False):
-        self._debug = debug
-        self.logger  = logging.getLogger(__name__)
-        self.__schema = schema
-        self.__validator = None
-        self.__file = fileName
+        # super().__init__(fileName, schema, debug)
+        return NotImplementedError("BiosourcePropertiesValidator coming soon")
