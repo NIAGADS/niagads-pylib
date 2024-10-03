91168b6f8efef32cf6506178433eaf3e66d9ba23
fossilfriend <egreenfest@gmail.com>
2024-10-03 15:55:56 -0400

    allow timeout to be set and caught

--- niagads/db/postgres/__init__.py
+++ niagads/db/postgres/__init__.py
@@ -1,2 +1,2 @@
 from .postgres_async import AsyncDatabase
-from .postgres_dbi import Database, DatabaseError, DataError, raise_pg_exception
+from .postgres_dbi import Database, DatabaseError, DataError, QueryCanceledError, raise_pg_exception
