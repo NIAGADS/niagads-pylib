c348c47f2986b7c01cbd26a08931f8e88c0cf381
fossilfriend <egreenfest@gmail.com>
2025-02-13 09:31:02 -0500

    indicate raw strings for regex patterns

--- niagads/utils/enums.py
+++ niagads/utils/enums.py
@@ -41,9 +41,9 @@ class RegularExpressions(CustomStrEnum, StrEnum):
     """
     commonly used regexps
     """
-    PUBMED_ID ='^([0-9]{8}|PMID:[0-9]{8})$'
-    MD5SUM = '^[a-fA-F0-9]{32}$'
-    DOI = '(10[.][0-9]{4,}(?:[.][0-9]+)*\/(?:(?!["&\'<>])\S)+)$'
-    FILE_SIZE = '^[.0-9]+\s?(K|M|G)$'
-    KEY_VALUE_PAIR = '^[^\s=]+=[^=;]+$' # key=value
+    PUBMED_ID = r'^([0-9]{8}|PMID:[0-9]{8})$'
+    MD5SUM = r'^[a-fA-F0-9]{32}$'
+    DOI = r'(10[.][0-9]{4,}(?:[.][0-9]+)*\/(?:(?!["&\'<>])\S)+)$'
+    FILE_SIZE = r'^[.0-9]+\s?(K|M|G)$'
+    KEY_VALUE_PAIR = r'^[^\s=]+=[^=;]+$' # key=value
 
