cb953a7f0ab74135d405de9f48b2122fad612fbc
fossilfriend <egreenfest@gmail.com>
2025-04-05 10:17:26 -0400

    sphinx extensions for annotated and typehints

--- docs/conf.py
+++ docs/conf.py
@@ -8,16 +8,16 @@ import os
 import sys
 
 
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath("../"))
 
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'NIAGADS-pylib'
-copyright = f'{date.today().year}, University of Pennsylvania'
-author = 'NIAGADS'
-release = '0.1'
+project = "NIAGADS-pylib"
+copyright = f"{date.today().year}, University of Pennsylvania"
+author = "NIAGADS"
+release = "0.0.1-alpha.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -25,16 +25,17 @@ release = '0.1'
 
 
 extensions = [
-    'autoclasstoc',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    "autoclasstoc",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_annotation",
+    "sphinx_autodoc_typehints",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # autodoc_member_order = 'bysource'
-
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -42,5 +43,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 # html_theme = 'alabaster'
 # html_theme = 'sphinx_rtd_theme'
-html_theme = 'cloud'
-html_static_path = ['_static']
+html_theme = "sphinxawesome_theme"
+html_static_path = ["_static"]
