6401637c7124b7a0469b967e346a8e584a5d32ac
fossilfriend <egreenfest@gmail.com>
2025-04-05 10:17:04 -0400

    custom -> case insensitive enum

--- components/niagads/metadata_validator/core.py
+++ components/niagads/metadata_validator/core.py
@@ -1,34 +1,46 @@
-from typing import List
+from enum import auto
+from typing import List, Annotated
 
 from niagads.csv_validator.core import CSVTableValidator
+from niagads.enums.core import CaseInsensitiveEnum
 from niagads.exceptions.core import ValidationError
 from niagads.list_utils.core import get_duplicates, list_to_string
+
+
+class MetadataFileType(CaseInsensitiveEnum):
+    """
+    Types of tabular metadata files
+    """
+
+    PARTICIPANT: Annotated[str, "participant or subject file"] = auto()
+    FILE_MANIFEST: Annotated[str, "file manifest; may map samples to files"] = auto()
+    SAMPLE: Annotated[str, "sample biocharacteristics"] = auto()
+    SDRF: Annotated[str, "sample-data-relationship file"] = auto()
+
 
 class FileManifestValidator(CSVTableValidator):
     """
     validate a file manifest in CSV format, with column names
     and 1 row per file
-    
+
     also compares against list of samples / biosources to make sure
     all biosources are known
     """
-    def __init__(self, fileName, schema, debug:bool=False):
+
+    def __init__(self, fileName, schema, debug: bool = False):
         self.__sampleReference: List[str] = None
-        self.__sampleField: str = 'sample_id'
+        self.__sampleField: str = "sample_id"
         super().__init__(fileName, schema, debug)
-        
-    
-    def set_sample_reference(self, sampleReference:List[str]):
+
+    def set_sample_reference(self, sampleReference: List[str]):
         self.__sampleReference = sampleReference
-        
-        
-    def set_sample_field(self, field:str):
+
+    def set_sample_field(self, field: str):
         self.__sampleField = field
-        
-        
-    def validate_samples(self, failOnError:bool=False):
+
+    def validate_samples(self, failOnError: bool = False):
         """
-        verifies that samples in the file manifest are 
+        verifies that samples in the file manifest are
         present in a reference list of samples
 
         Args:
@@ -37,40 +49,43 @@ class FileManifestValidator(CSVTableValidator):
         Returns:
             list of invalid samples
         """
-        
+
         sampleIds = self.get_field_values(self.__sampleField, dropNulls=True)
         sampleSet = set(sampleIds)
-        referenceSet  = set(self.__sampleReference)
-        
+        referenceSet = set(self.__sampleReference)
+
         invalidSamples = list(sampleSet - referenceSet)
         missingSamples = list(referenceSet - sampleSet)
-        
+
         if len(invalidSamples) == 0 and len(missingSamples) == 0:
             return True
-        
+
         messages = []
         if len(invalidSamples) > 0:
-            error = ValidationError("Invalid samples found: " + list_to_string(invalidSamples, delim=', '))
+            error = ValidationError(
+                "Invalid samples found: " + list_to_string(invalidSamples, delim=", ")
+            )
             if failOnError:
                 raise error
             else:
-                messages.append(f'ERROR: {error.message}')
-                
+                messages.append(f"ERROR: {error.message}")
+
         if len(missingSamples) > 0:
-            msg = "WARNING: Files not found for all samples: " + list_to_string(missingSamples, delim=', ')
+            msg = "WARNING: Files not found for all samples: " + list_to_string(
+                missingSamples, delim=", "
+            )
             messages.append(msg)
-        
+
         return messages
 
-
-    def run(self, failOnError:bool=False):
+    def run(self, failOnError: bool = False):
         """
         run validation on each row
 
         wrapper of TableValidator.run that also does sample validation
         """
         result = super().run(failOnError)
-        
+
         if self.__sampleReference is not None:
             sampleValidationResult = self.validate_samples(failOnError)
             if not isinstance(sampleValidationResult, bool):
@@ -79,49 +94,48 @@ class FileManifestValidator(CSVTableValidator):
                     return result + sampleValidationResult
                 else:
                     return [sampleValidationResult]
-        
-        return result        
-            
+
+        return result
+
 
 class BiosourcePropertiesValidator(CSVTableValidator):
     """
     validate biosource properties in a CSV format file, with column names
     and 1 row per sample or participant
-    
+
     """
-    def __init__(self, fileName, schema, debug:bool=False):
+
+    def __init__(self, fileName, schema, debug: bool = False):
         super().__init__(fileName, schema, debug)
-        self.__biosourceID = 'sample_id'
+        self.__biosourceID = "sample_id"
         self.__requireUniqueIDs = False
-    
-    def set_biosource_id(self, idField: str, requireUnique: bool=False):
+
+    def set_biosource_id(self, idField: str, requireUnique: bool = False):
         self.__biosourceID = idField
         self.__requireUniqueIDs = requireUnique
-        
 
     def require_unique_identifiers(self):
         self.__requireUniqueIDs = True
 
-        
-    def validate_unqiue_identifiers(self, failOnError:bool=False):
+    def validate_unqiue_identifiers(self, failOnError: bool = False):
         duplicates = get_duplicates(self.get_biosource_ids())
         if len(duplicates) > 0:
-            error = ValidationError(f'Duplicate biosource identifiers found: {list_to_string(duplicates, delim=", ")}')
+            error = ValidationError(
+                f'Duplicate biosource identifiers found: {list_to_string(duplicates, delim=", ")}'
+            )
             if failOnError:
                 raise error
             else:
-                return f'ERROR: {error.message}'
+                return f"ERROR: {error.message}"
         return True
-        
-    
+
     def get_biosource_ids(self):
         """
         extract biosource IDs
         """
         return self.get_field_values(self.__biosourceID)
-    
-    
-    def run(self, failOnError:bool=False):
+
+    def run(self, failOnError: bool = False):
         """
         run validation on each row
 
@@ -135,7 +149,5 @@ class BiosourcePropertiesValidator(CSVTableValidator):
                     return result + sampleValidationResult
                 else:
                     return [sampleValidationResult]
-        
-        return result        
-    
-    
+
+        return result
--- components/niagads/postgres_dbi/prepared_statement.py
+++ components/niagads/postgres_dbi/prepared_statement.py
@@ -3,105 +3,104 @@
 
 from typing import Dict, List, Union
 
-from niagads.enums.core import CustomStrEnum, UppercaseStrEnum, auto
+from niagads.enums.core import CaseInsensitiveEnum, auto
 from psycopg2.sql import SQL as psSQL
 from psycopg2.sql import Identifier as psIdentifier
 from psycopg2.sql import Placeholder as psPlaceholder
 
 
-class Statement(CustomStrEnum, UppercaseStrEnum):
+class Statement(CaseInsensitiveEnum):
     SELECT = auto()
     INSERT = auto()
     UPDATE = auto()
-    
 
-class PreparedStatement():
+
+class PreparedStatement:
     """
     leverage pyscopg2 sql package (https://www.psycopg.org/docs/sql.html)
-    to generate a prepared statements   
+    to generate a prepared statements
     """
-    
+
     def __init__(self, schemaName, tableName):
-        self.__table:str = tableName
-        self.__schema:str = schemaName
-        self.__useNamedPlaceholders:bool = False
+        self.__table: str = tableName
+        self.__schema: str = schemaName
+        self.__useNamedPlaceholders: bool = False
         self.__caseSensitiveNaming: bool = False
-        
-        
+
     def case_sensitive_naming(self, flag=True):
-        """ table/schema names are case sensitive"""
+        """table/schema names are case sensitive"""
         self.__caseSensitiveNaming = flag
-        
-        
+
     def use_named_placeholders(self, flag=True):
         """
         placholders represented as %(foo)s, instead of %s
         """
         self.__useNamedPlaceholders = flag
-        
-        
+
     def __format_table_name(self):
         tableName = self.__table if self.__caseSensitiveNaming else self.__table.lower()
-        schemaName = self.__schema if self.__caseSensitiveNaming else self.__schema.lower()
+        schemaName = (
+            self.__schema if self.__caseSensitiveNaming else self.__schema.lower()
+        )
 
         return schemaName, tableName
-    
-    
-    def select(self, fields: list, filters: dict, returnAsString:bool=False):
+
+    def select(self, fields: list, filters: dict, returnAsString: bool = False):
         """
         leverage pyscopg2 sql package (https://www.psycopg.org/docs/sql.html)
         to generate a prepared select statement from a list of fields and dict of field_names:value pairs
         for filtering
-        
+
         Args:
-            
-        
+
+
         Returns:
-            prepared insert statement in the format: 
+            prepared insert statement in the format:
                 SELECT f1, f2, f3, f4 FROM "schema"."table" WHERE f5 = 'x'
 
         """
         raise NotImplementedError()
-    
-    
-    def insert(self, fields: Union[Dict, List], returnAsString:bool=False):
+
+    def insert(self, fields: Union[Dict, List], returnAsString: bool = False):
         """
         leverage pyscopg2 sql package (https://www.psycopg.org/docs/sql.html)
         to generate a prepared insert statement from a dict of field_names:value pairs
-        
+
         if fields is a dict, field names are sorted; if array, original order is kept
 
         Args:
             fields (dict|list): list of field names or dict of field_name:value pairs
             returnAsString (bool, optional): flag return as string instead of composed SQL object.   Defaults to False.
-        
+
         Returns:
-            prepared insert statement in the format: 
+            prepared insert statement in the format:
                 insert into "schema"."table" ("foo", "bar", "baz") values (%s, %s, %s)
             or, with named placeholders:
                 insert into "schema.table" ("foo", "bar", "baz") values (%(foo)s, %(bar)s, %(baz)s)
-            
+
         """
 
         fieldNames = fields
         if isinstance(fields, dict):
             fieldNames = list(fields.keys())
             fieldNames.sort()
-            
+
         # TODO - named placeholders
         if self.__useNamedPlaceholders:
             raise NotImplementedError("TODO - named placeholders")
-        
-        formattedPlaceholders = psSQL(', ').join(psPlaceholder() * len(fieldNames))
+
+        formattedPlaceholders = psSQL(", ").join(psPlaceholder() * len(fieldNames))
 
         schemaName, tableName = self.__format_table_name()
-        statement = psSQL("INSERT INTO {table} ({fields}) values ({placeholders})").format(
+        statement = psSQL(
+            "INSERT INTO {table} ({fields}) values ({placeholders})"
+        ).format(
             table=psIdentifier(schemaName, tableName),
-            fields=psSQL(', ').join(map(psIdentifier, fieldNames)),
-            placeholders=formattedPlaceholders)
-        
+            fields=psSQL(", ").join(map(psIdentifier, fieldNames)),
+            placeholders=formattedPlaceholders,
+        )
+
         # TODO - as string
         if returnAsString:
             raise NotImplementedError("TODO - return as string")
         return statement
-
