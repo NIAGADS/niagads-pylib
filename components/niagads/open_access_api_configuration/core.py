ce554f6bf85922863c3c5d953d64824002963957
fossilfriend <egreenfest@gmail.com>
2025-04-09 13:48:56 -0400

    shared constants

---
+++ components/niagads/open_access_api_configuration/core.py
@@ -0,0 +1,17 @@
+# Settings for Open Access API microservices
+
+# cache db
+# int or float in seconds specifying maximum timeout for the operations to last. By default (aiocache) its 5. Use 0 or None if you want to disable it.
+CACHEDB_TIMEOUT = 5
+
+# http client
+HTTP_CLIENT_TIMEOUT = 30  # timeout in seconds
+
+# pagination
+DEFAULT_PAGE_SIZE = 5000
+MAX_NUM_PAGES = 10
+
+# regular expressions
+SHARD_PATTERN = r"chr(\d{1,2}|[XYM]|MT)"
+
+DEFAULT_NULL_STRING = "NA"
