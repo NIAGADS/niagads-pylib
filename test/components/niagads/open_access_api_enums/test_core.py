2055e531424fb4904c8e3c3fac5ad6f44e0d980f
fossilfriend <egreenfest@gmail.com>
2025-04-09 05:49:21 -0400

    enums/types test

---
+++ test/components/niagads/open_access_api_enums/test_core.py
@@ -0,0 +1,5 @@
+from niagads.open_access_api_enums import core
+
+
+def test_sample():
+    assert core is not None
---
+++ test/components/niagads/open_access_api_types/test_core.py
@@ -0,0 +1,5 @@
+from niagads.open_access_api_types import core
+
+
+def test_sample():
+    assert core is not None
