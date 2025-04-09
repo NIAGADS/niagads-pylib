19fdb54fd470a08f121b31567d232caaf74ae369
fossilfriend <egreenfest@gmail.com>
2025-04-09 13:49:07 -0400

    Assembly parameter

---
+++ components/niagads/open_access_api_parameters/core.py
@@ -0,0 +1,8 @@
+from niagads.genome.core import Assembly
+from niagads.open_access_api_enums.core import EnumParameter
+
+
+class AssemblyParameter(Assembly, EnumParameter):
+    """add EnumParameter validation to Assembly Enum"""
+
+    pass
