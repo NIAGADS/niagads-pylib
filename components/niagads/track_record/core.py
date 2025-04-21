1d4eef204fc51245e443f7a3b96baadfa05cb400
fossilfriend <egreenfest@gmail.com>
2025-04-21 10:35:54 -0400

    track metdata table

--- components/niagads/track_record/core.py
+++ components/niagads/track_record/core.py
@@ -5,31 +5,43 @@ from typing import Optional
 from niagads.genome.core import Assembly
 from niagads.list_utils.core import list_to_string
 from niagads.open_access_api_configuration.core import DataStore
-from niagads.track_record.models.properties import BiosampleCharacteristics
-from sqlalchemy import TEXT, Column, Enum, String
+from niagads.track_record.models.properties import (
+    BiosampleCharacteristics,
+    ExperimentalDesign,
+    FileProperties,
+    Provenance,
+)
+from sqlalchemy import TEXT, Column, Enum, MetaData, String
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.schema import CheckConstraint
 
 
-class Base(DeclarativeBase):
-    pass
+class TrackBase(DeclarativeBase):
+    """base class for the track database models"""
+
+    metadata = MetaData(schema="track")
 
 
-class Track(Base):
+class Track(TrackBase):
     __tablename__ = "metadataentry"
     __table_args__ = (
         CheckConstraint(
             f"genome_build in ({list_to_string(Assembly.list(), quote=True, delim=', ')})",
             name="check_genome_build",
         ),
-        {"schema": "track"},
+        CheckConstraint(
+            f"data_store in ({list_to_string(DataStore.list(), quote=True, delim=', ')})",
+            name="check_data_store",
+        ),
     )
 
     metadata_entry_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
 
     track_id: Mapped[str] = mapped_column(unique=True, index=True)
-    data_store: DataStore
+    data_store: str = Column(
+        Enum(DataStore, native_enum=False), nullable=False, index=True
+    )
     name: Mapped[str]
     description: Mapped[str] = mapped_column(String(2000))
 
@@ -40,103 +52,12 @@ class Track(Base):
     feature_type: Mapped[str] = mapped_column(String(50), index=True)
     is_download_only: Mapped[bool] = mapped_column(default=False, index=True)
 
-    searchable_text: str = Column(TEXT, index=True)
+    searchable_text: Mapped[str] = mapped_column(TEXT, index=True)
 
     is_shard: Mapped[Optional[bool]]
     shard_root_track_id: Mapped[Optional[str]] = mapped_column(index=True)
 
-    # biosample
     biosample_characteristics: Mapped[BiosampleCharacteristics] = mapped_column(JSONB)
-
-    """
-    # experimental design
-    experimental_design: ExperimentalDesign | None = Field(sa_column=Column(JSONB))
-
-    # provenance
-    provenance: Provenance | None = Field(sa_column=Column(JSONB))
-
-    # track_properties
-    properties: TrackProperties | None = Field(sa_column=Column(JSONB))
-    
-    file_name: Optional[str]
-    url: Optional[str]
-    md5sum: Optional[str]
-
-    bp_covered: Optional[int] = Field(sa_column=Column(BigInteger()))
-    number_of_intervals: Optional[int] = Field(sa_column=Column(BigInteger()))
-    file_size: Optional[int]
-
-    file_format: Optional[str]
-    file_schema: Optional[str]
-    release_date: Optional[date] = Field(sa_column=Column(TIMESTAMP(timezone=False)))
-
-    # FIXME: move to Provenance
-    @computed_field
-    @property
-    def data_source_url(self) -> str:
-        dsKey = (
-            self.data_source + "|" + self.data_source_version
-            if self.data_source_version is not None
-            else self.data_source
-        )
-        try:
-            return getattr(DATASOURCE_URLS, dsKey)
-        except (
-            Exception
-        ) as e:  # TODO: error reporting to admins b/c this is a missing data problem
-            return self.data_source
-
-    # =================================
-    # GENOME BROWSER FIELDS
-    # =================================
-
-    # FIXME: create genome browser properties object and populate
-
-    @computed_field
-    @property
-    def IGV_browser_config(self) -> IGVBrowserConfig:
-        # index_url, browser_track_name, browser_track_category, browser_track_format, track_type
-    
-    @computed_field
-    @property
-    def index_url(self) -> str:
-        if self.url.endswith(".gz"):
-            return self.url + ".tbi"
-        else:
-            return None
-    
-    @computed_field
-    @property
-    def browser_track_name(self) -> str:
-        return (
-            self.track_id
-            + ": "
-            + self.name.replace(
-                f"{self.feature_type} {self.feature_type}", self.feature_type
-            )
-        )
-
-    @computed_field
-    @property
-    def browser_track_category(
-        self,
-    ) -> str:  # TODO: be more specific? e.g., data category?
-        return "Functional Genomics"
-
-    @computed_field
-    @property
-    def browser_track_format(self) -> str:
-        if self.file_schema is None:
-            return "bed"
-        schema = self.file_schema.split("|")
-        return schema[0]
-
-    @computed_field
-    @property
-    def browser_track_type(self) -> str:
-        trackType = "annotation"
-        if "|" in self.file_schema:
-            schema = self.file_schema.split("|")
-            trackType = schema[1]
-        return trackType
-    """
+    experimental_design: Mapped[ExperimentalDesign] = mapped_column(JSONB)
+    provenance: Mapped[Provenance] = mapped_column(JSONB)
+    properties: Mapped[FileProperties] = mapped_column(JSONB)
--- components/niagads/track_record/models/properties.py
+++ components/niagads/track_record/models/properties.py
@@ -1,9 +1,15 @@
-
 from datetime import date
 from typing import List, Optional
-from pydantic import BaseModel, Field
 
-class ExperimentalDesign(BaseModel):
+from niagads.open_access_api_configuration.resource_links import DATASOURCE_URLS
+from niagads.open_access_api_models.core import NullFreeModel
+from niagads.string_utils.regular_expressions import RegularExpressions
+from pydantic import BaseModel, Field, computed_field
+from sqlalchemy import Null
+from zmq import PUB
+
+
+class ExperimentalDesign(NullFreeModel):
     antibody_target: Optional[str] = None
     assay: Optional[str] = None
     analysis: Optional[str] = None
@@ -11,31 +17,64 @@ class ExperimentalDesign(BaseModel):
     data_category: Optional[str] = None
     output_type: Optional[str] = None
     is_lifted: Optional[bool] = False
-    
-
-class BiosampleCharacteristics(BaseModel):
-    system_category: Optional[str] = None
-    tissue_category: Optional[str] = None
-    # biosample_display: Optional[str] = None
-    # biosample_summary: Optional[str] = None
-    biosample_term: Optional[str] = Field(default=None, description='mapped ontology term')
-    # biosample_term_id: Optional[str] = Field(default=None, description='mapped ontology term identifier')
-    life_stage: Optional[str] = Field(default=None, description='donor/sample life stage: adult, fetal, embryo')
 
 
-class Provenance(BaseModel):
-    data_source: Optional[str] = None
+class BiosampleCharacteristics(NullFreeModel):
+    system: Optional[str] = None
+    tissue: Optional[str] = None
+    biosample_term: Optional[str] = Field(
+        default=None, description="mapped ontology term"
+    )
+    biosample_term_id: Optional[str] = Field(
+        default=None, description="mapped ontology term identifier"
+    )
+    life_stage: Optional[str] = Field(
+        default=None, description="donor/sample life stage: adult, fetal, embryo"
+    )
+
+
+class Provenance(NullFreeModel):
+    data_source: str
+    data_source_version: Optional[str] = None
+
+    download_date: Optional[date] = None
     download_url: Optional[str] = None
-    release_date: Optional[date] = None
-    
 
-class DSSAccession(Provenance):
-    data_source: str = "NIAGADS DSS"
+    release_date: Optional[date] = None
+
+    study_or_project: Optional[str] = None
     accession: str
-    pubmed_id: Optional[str] = None
-    attribution: Optional[str] = None
+    pubmed_id: Optional[str] = Field(regex=RegularExpressions.PUBMED_ID, default=None)
+    doi: Optional[str] = Field(regex=RegularExpressions.DOI, default=None)
     consortia: Optional[str] = None
-    # TODO: collections?
-    
-    
-    
+    attribution: Optional[str] = None
+
+    @computed_field
+    @property
+    def data_source_url(self) -> str:
+        dsKey = (
+            self.data_source + "|" + self.data_source_version
+            if self.data_source_version is not None
+            else self.data_source
+        )
+        try:
+            return getattr(DATASOURCE_URLS, dsKey)
+
+        except KeyError:
+            raise KeyError(
+                f"Data source URL not found for {dsKey}. Please add to DATASOURCE_URLS."
+            )
+
+
+class FileProperties(NullFreeModel):
+    file_name: Optional[str] = None
+    url: Optional[str] = None
+    md5sum: Optional[str] = Field(regex=RegularExpressions.MD5SUM, default=None)
+
+    bp_covered: Optional[int] = None
+    number_of_intervals: Optional[int] = None
+    file_size: Optional[int] = None
+
+    file_format: Optional[str] = None
+    file_schema: Optional[str] = None
+    release_date: Optional[date] = None
