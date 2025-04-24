d341cbd8c10da684b267df61190e21d925672fac
fossilfriend <egreenfest@gmail.com>
2025-04-24 15:45:01 -0400

    placeholder filer service - broken needs to be updated to use app factory

--- bases/niagads/open_access_filer_api/core.py
+++ bases/niagads/open_access_filer_api/core.py
@@ -1,45 +1,67 @@
-from fastapi import APIRouter, Depends
-from typing import Annotated
-from sqlalchemy.ext.asyncio import AsyncSession
+from asgi_correlation_id import CorrelationIdMiddleware
+from fastapi import APIRouter, FastAPI
 
-from api.common.services.metadata_query import MetadataQueryService
-from api.models.response_model_properties import RequestDataModel
-from api.models.base_response_models import ResponseModel
+from niagads.open_access_api_common.exception_handlers import (
+    add_not_implemented_exception_handler,
+    add_runtime_exception_handler,
+    add_system_exception_handler,
+    add_validation_exception_handler,
+)
+from niagads.open_access_filer_api.routes.root import router as InfoRouter
 
-from api.routes.filer.common.constants import ROUTE_NAME, ROUTE_TAGS, ROUTE_PREFIX
-from api.routes.filer.dependencies.parameters import ROUTE_SESSION_MANAGER
-
-from api.routes.filer.routes.track import router as TrackRouter
-from api.routes.filer.routes.metadata import router as MetadataRouter
-from api.routes.filer.routes.data import router as DataRouter
-from api.routes.filer.routes.service import router as ServiceRouter
-from api.routes.filer.routes.collection import router as CollectionRouter
-from api.routes.filer.routes.qtls import router as QTLRouter
-
-router = APIRouter(
-    prefix=ROUTE_PREFIX,
-    tags=ROUTE_TAGS,
-    responses=RESPONSES,
+app = FastAPI(
+    title="NIAGADS Open Access API",
+    description="an application programming interface (API) that provides programmatic access to Open Access resources at the NIA Genetics of Alzheimer's Disease Data Storage Site (NIAGADS)",
+    summary="NIAGADS Open Access API",
+    version="0.0.1",  # FIXME: get from settings
+    # terms_of_service="http://example.com/terms/",
+    # contact={"name": "NIAGADS Support", "email": get_settings().ADMIN_EMAIL},
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+    # servers=[{"url": get_settings().API_PUBLIC_URL}],
+    swagger_ui_parameters={
+        "apisSorter": "alpha",
+        "operationsSorter": "alpha",
+        "tagsSorter": "alpha",
+    },
+    docs_url=None,
+    redoc_url=None,
+    # default_response_class=ResponseModel,
+    # openapi_tags=ROUTE_TAGS,
 )
 
-tags=['Route Information']
-@router.get("/", name="about", response_model=ResponseModel, tags=tags,
-            description="brief summary about the " + ROUTE_NAME)
-async def read_root(
-    session: Annotated[AsyncSession, Depends(ROUTE_SESSION_MANAGER)],
-    requestData: RequestDataModel = Depends(RequestDataModel.from_request)
-        )-> ResponseModel:
-    
-    result = await MetadataQueryService(session, dataStore=[DataStore.FILER, DataStore.SHARED]).get_track_count()
-    return ResponseModel(data = {"database": "FILER", "number of tracks": result}, request=requestData)
+app.add_middleware(CorrelationIdMiddleware, header_name="X-Request-ID")
+
+# TODO make CORS conditional on production v dev
+app.add_middleware(
+    CORSMiddleware,
+    # allow_origins=[get_settings().API_PUBLIC_URL],
+    allow_origins=["*"],
+    allow_origin_regex=r"https://.*\.niagads\.org",
+    # allow_credentials=False,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
+
+add_runtime_exception_handler(app)
+add_validation_exception_handler(app)
+add_system_exception_handler(app)
+add_not_implemented_exception_handler(app)
 
 
 # --------------------------------------------------------------
 # CHIILD ROUTES
 # --------------------------------------------------------------
-router.include_router(TrackRouter)
-router.include_router(MetadataRouter)
-router.include_router(DataRouter)
-router.include_router(ServiceRouter)
-router.include_router(CollectionRouter)
-router.include_router(QTLRouter)
+
+app.include_router(InfoRouter)
+"""
+app.include_router(TrackRouter)
+app.include_router(MetadataRouter)
+app.include_router(DataRouter)
+app.include_router(ServiceRouter)
+app.include_router(CollectionRouter)
+app.include_router(QTLRouter)
+"""
