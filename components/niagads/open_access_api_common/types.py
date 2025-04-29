2728bfbd5c61a3d73e871eece90f5e8870986170
fossilfriend <egreenfest@gmail.com>
2025-04-29 14:11:32 -0400

    replace extended enum

--- components/niagads/open_access_api_common/types.py
+++ components/niagads/open_access_api_common/types.py
@@ -1,18 +1,24 @@
-from enum import StrEnum, auto
+from enum import auto
 from typing import Annotated, Any, Dict, List, Optional, Union
 
 from niagads.enums.core import CaseInsensitiveEnum
-from niagads.genome.core import GenomicFeatureType
 from niagads.utils.regular_expressions import RegularExpressions
-from pydantic import BaseModel, Field, constr, model_validator
+from pydantic import BaseModel, Field, model_validator
 
 T_JSON = Union[Dict[str, Any], List[Any], int, float, str, bool, None]
 
 T_PubMedID = Annotated[str, Field(pattern=RegularExpressions.PUBMED_ID)]
 
 
-class RecordType(GenomicFeatureType):
+class RecordType(CaseInsensitiveEnum):
+    GENE = auto()
+    VARIANT = auto()
+    # STRUCTURAL_VARIANT = auto()
+    SPAN = auto()
     TRACK = auto()
+
+    def __str__(self):
+        return self.name.lower()
 
 
 class Range(BaseModel):
