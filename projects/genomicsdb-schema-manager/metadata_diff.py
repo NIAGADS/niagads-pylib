501afbb0a4b80146d9cc176f6dc2f6399f5fa498
fossilfriend <egreenfest@gmail.com>
2025-05-05 16:03:34 -0400

    test script

---
+++ projects/genomicsdb-schema-manager/metadata_diff.py
@@ -0,0 +1,18 @@
+## print diff in metadata schema when model compared to database
+## test script
+import pprint
+
+from alembic.autogenerate import compare_metadata
+from alembic.migration import MigrationContext
+from sqlalchemy import create_engine
+
+from database.schemas import Schema
+from database.config import Settings
+
+metadata: Schema = Schema.base("metadata").metadata
+engine = create_engine(Settings.from_env().DATABASE_URI)
+
+mc = MigrationContext.configure(engine.connect())
+
+diff = compare_metadata(mc, metadata)
+pprint.pprint(diff, indent=2, width=20)
