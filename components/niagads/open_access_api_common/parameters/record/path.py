e2d686d7c775112e41b7002de9d697978ef965b0
fossilfriend <egreenfest@gmail.com>
2025-06-18 01:52:08 -0400

    gene & variant path params

--- components/niagads/open_access_api_common/parameters/record/path.py
+++ components/niagads/open_access_api_common/parameters/record/path.py
@@ -3,6 +3,10 @@
 from typing import Optional
 from fastapi import Path, Query
 from niagads.exceptions.core import ValidationError
+from niagads.genome.core import GenomicFeatureType
+from niagads.open_access_api_common.models.records.features.genomic import (
+    GenomicFeature,
+)
 from niagads.utils.string import sanitize
 
 
@@ -18,11 +22,17 @@ async def collection_param(
 
 
 # genomic features
-async def variant_param(variant: str = Query(regex="", description="")):
-    return True
+async def variant_param(
+    variant: str = Path(
+        description="variant record identifier; refSNP ID or positional allele (chr:pos:ref:alt)"
+    ),
+):
+    return GenomicFeature(
+        feature_id=sanitize(variant), feature_type=GenomicFeatureType.VARIANT
+    )
 
 
-async def query_collection_name(
-    collection: Optional[str] = Query(default=None, description="track collection name")
-) -> str:
-    return sanitize(collection)
+async def gene_param(gene: str = Path(description="")):
+    return GenomicFeature(
+        feature_id=sanitize(gene), feature_type=GenomicFeatureType.GENE
+    )
