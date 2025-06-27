650393e3b6af9455dcb95b43ccbd3e06268fbc71
fossilfriend <egreenfest@gmail.com>
2025-06-27 11:12:53 -0400

    make custom model abstract

--- components/niagads/common/models/core.py
+++ components/niagads/common/models/core.py
@@ -1,18 +1,42 @@
 from typing import TypeVar
 from niagads.utils.dict import prune
-from pydantic import BaseModel, ConfigDict, model_serializer, model_validator
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    model_serializer,
+)
+from abc import ABC, abstractmethod
 
 
 class NullFreeModel(BaseModel):
     """a pydantic model where attributes with NULL values (e.g., None, 'NULL') are removed during serialization"""
+
+    # note: this ignores the model_config b/c it doesn't run model_dump()
 
     @model_serializer()
     def serialize_model(self, values, **kwargs):
         return prune(dict(self), removeNulls=True)
 
 
-class CustomBaseModel(BaseModel):
+class CustomBaseModel(BaseModel, ABC):
     model_config = ConfigDict(serialize_by_alias=True, use_enum_values=True)
+
+    @abstractmethod
+    def as_info_string(self):
+        """serializes model as field=value;... info string"""
+        raise NotImplementedError(
+            "This is an abstract method; must be implemented for each child model."
+        )
+
+    @abstractmethod
+    def serialize_as_table(self):
+        """returns a {columns: , data: } object for a view table"""
+        raise NotImplementedError(
+            "This is an abstract method; must be implemented for each child model."
+        )
+
+    def as_null_free_json(self):
+        return prune(self.model_dump(), removeNulls=True)
 
 
 T_CustomBaseModel = TypeVar("T_RowModel", bound=CustomBaseModel)
