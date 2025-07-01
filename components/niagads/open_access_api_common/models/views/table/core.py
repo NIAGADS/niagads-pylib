82f031b0f9c34db4f14f1851dd7cda0d2e0bcaa8
fossilfriend <egreenfest@gmail.com>
2025-07-01 11:40:19 -0400

    fix import

--- components/niagads/open_access_api_common/models/views/table/core.py
+++ components/niagads/open_access_api_common/models/views/table/core.py
@@ -6,13 +6,13 @@ FIXME: remove front end rendering and place in configuration JSON
 from typing import Optional
 
 
-from niagads.common.models.table.core import Table
+from niagads.common.models.views.table import Table
 from niagads.open_access_api_common.models.views.core import ViewResponse
 
 
 class TableViewModel(Table):
     id: str
-    title: Optional[str]
+    title: Optional[str] = None
 
 
 class TableViewResponse(ViewResponse):
