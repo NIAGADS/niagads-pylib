f08d3fb44ee6531550de2fe5503b891edce878ed
fossilfriend <egreenfest@gmail.com>
2025-07-01 18:28:44 -0400

    update search response

--- components/niagads/open_access_api_common/models/records/search.py
+++ components/niagads/open_access_api_common/models/records/search.py
@@ -3,19 +3,26 @@ from typing import List
 from niagads.open_access_api_common.models.core import Entity
 from niagads.open_access_api_common.models.records.core import RowModel
 from niagads.open_access_api_common.models.response.core import GenericResponse
-from pydantic import AliasChoices, Field
+from pydantic import Field
 
 
 class RecordSearchResult(RowModel):
-    id: str = Field(
-        validation_alias=AliasChoices("primary_key")
-    )  # primary_key (identifier) for the record (e.g., ensembl ID)
+    primary_key: str = Field(
+        serialization_alias="id",
+        title="Record ID",
+        description="unique record identifier",
+    )
     description: str  # descriptive text
-    display: str  # display id (e.g. gene symbol)
-    record_type: Entity
-    matched_term: str
+    display: str = Field(title="Display ID")  # display id (e.g. gene symbol)
+    record_type: Entity = Field(title="Record Type")
+    matched_term: str = Field(title="Matched", description="matched term or phrase")
     match_rank: int
 
 
 class RecordSearchResultResponse(GenericResponse):
     data: List[RecordSearchResult]
+
+    def to_text(self, inclHeader=False, nullStr=...):
+        raise NotImplementedError(
+            "TEXT formatted output not available for a search result response."
+        )
