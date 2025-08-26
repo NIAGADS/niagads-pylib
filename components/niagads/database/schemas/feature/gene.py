cc4dfe86312c37c5f04daac5a094ca30ad791ac5
fossilfriend <egreenfest@gmail.com>
2025-08-26 14:18:22 -0400

    add enum based fields

--- components/niagads/database/schemas/feature/gene.py
+++ components/niagads/database/schemas/feature/gene.py
@@ -3,27 +3,39 @@
 from enum import auto
 from typing import Any, List, Optional
 
-from niagads.database.core import ModelDumpMixin
+from niagads.database.core import ModelDumpMixin, enum_constraint
 from niagads.database.schemas.feature.base import FeatureSchemaBase
+from niagads.database.schemas.feature.composite_attributes import (
+    GOAnnotation,
+    PathwayAnnotation,
+)
+from niagads.genome.core import Human
 from niagads.utils.regular_expressions import RegularExpressions
-from sqlalchemy import ARRAY, TEXT, Column, Enum, Index, String
-from sqlalchemy.dialects.postgresql import INT8RANGE, JSONB
+from sqlalchemy import Column, Enum, Index
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.schema import CheckConstraint
 from sqlalchemy_utils import LtreeType
+from sqlalchemy.dialects.postgresql import INT4RANGE, JSONB
 
 
 class Gene(ModelDumpMixin, FeatureSchemaBase):
     __tablename__ = "gene"
     __table_args__ = (
+        enum_constraint("shard_chromosome", Human),
         CheckConstraint(
             f"ensembl_id ~ '{RegularExpressions.ENSEMBL_GENE_ID}'",
             name="ensembl_id_format_check",
         ),
+        Index("ix_gene_location", "location", postgresql_using="gist"),
         Index("ix_gene_bin_index", "bin_index", postgresql_using="gist"),
     )
 
     gene_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     ensembl_id: Mapped[str] = mapped_column(uniuqe=True, index=True)
-    chromosome: 
+    chromosome: str = Column(Enum(Human, native_enum=False))
     bin_index: Mapped[str] = mapped_column(LtreeType)
+    location: Mapped[Any] = mapped_column(INT4RANGE)
+    go_annotation: Mapped[Optional[GOAnnotation]] = mapped_column(
+        JSONB(none_as_null=True)
+    )
+    pathway_membership: Mapped[Optional[PathwayAnnotation]]
