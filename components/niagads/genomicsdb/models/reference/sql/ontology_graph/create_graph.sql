0fa0eb791c0b3d8f8e3fb2ab0bd1ce5a91747bd2
fossilfriend <egreenfest@gmail.com>
2025-11-10 22:37:42 -0500

    add restrictions

--- components/niagads/genomicsdb/models/reference/sql/ontology_graph/create_graph.sql
+++ components/niagads/genomicsdb/models/reference/sql/ontology_graph/create_graph.sql
@@ -2,7 +2,7 @@ RETURNS void AS $$
 BEGIN
     CREATE GRAPH IF NOT EXISTS Ontology;
 
-    CREATE VERTEX TYPE IF NOT EXISTS Reference.Ontology.term (
+    CREATE VERTEX TYPE IF NOT EXISTS Reference.Ontology.Term (
         term_id VARCHAR(32) PRIMARY KEY,       -- e.g. GO:0006915
         term_iri VARCHAR(150),                      -- full uri
         term VARCHAR(512),                     -- the term
@@ -11,31 +11,18 @@ BEGIN
         synonyms TEXT[],                       -- synonyms can be many / long
         is_deprecated BOOLEAN DEFAULT FALSE,
         is_placeholder BOOLEAN DEFAULT FALSE,  -- placeholder vertex
-        term_category VARCHAR(16) NOT NULL
-            CHECK (term_category IN ('CLASS','PROPERTY','INDIVIDUAL'))
+        --term_category VARCHAR(16) NOT NULL
+        --    CHECK (term_category IN ('CLASS','PROPERTY','INDIVIDUAL'))
 
     );
     
+
     -- Generic restriction node
     CREATE VERTEX TYPE IF NOT EXISTS Reference.Ontology.restriction (
         restriction_id SERIAL PRIMARY KEY
     );
 
-    -- Example usage:
-    /*
-    <owl:Class rdf:about="http://purl.obolibrary.org/obo/SO_0000016">
-        <rdfs:subClassOf rdf:resource="http://purl.obolibrary.org/obo/SO_0001660"/>
-        <rdfs:subClassOf>
-            <owl:Restriction>
-                <owl:onProperty rdf:resource="http://purl.obolibrary.org/obo/so#part_of"/>
-                <owl:someValuesFrom rdf:resource="http://purl.obolibrary.org/obo/SO_0001669"/>
-            </owl:Restriction>
-        </rdfs:subClassOf>
-    </owl:Class>
-    */
-    -- (SO_0000016, subClassOf, restriction_node)
-    -- (restriction_node, onProperty, part_of)
-    -- (restriction_node, someValuesFrom, SO_0001669)
+
 
     CREATE EDGE TYPE IF NOT EXISTS Reference.Ontology.triple (
         predicate VARCHAR(32) NOT NULL         -- must point to a term_id
@@ -54,4 +41,20 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+
+-- Example RESTRICTION usage:
+/*
+<owl:Class rdf:about="http://purl.obolibrary.org/obo/SO_0000016">
+    <rdfs:subClassOf rdf:resource="http://purl.obolibrary.org/obo/SO_0001660"/>
+    <rdfs:subClassOf>
+        <owl:Restriction>
+            <owl:onProperty rdf:resource="http://purl.obolibrary.org/obo/so#part_of"/>
+            <owl:someValuesFrom rdf:resource="http://purl.obolibrary.org/obo/SO_0001669"/>
+        </owl:Restriction>
+    </rdfs:subClassOf>
+</owl:Class>
+*/
+-- (SO_0000016, subClassOf, restriction_node)
+-- (restriction_node, onProperty, part_of)
+-- (restriction_node, someValuesFrom, SO_0001669)
 
