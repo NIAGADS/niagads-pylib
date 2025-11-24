db601e0a3b78f4ed836d2249c8b27499210dab7a
fossilfriend <egreenfest@gmail.com>
2025-11-24 17:05:02 -0500

    transformation logic

--- bases/niagads/genomicsdb_service/data_migration/migrate_legacy_gwas_to_hipFG_standard.py
+++ bases/niagads/genomicsdb_service/data_migration/migrate_legacy_gwas_to_hipFG_standard.py
@@ -3,29 +3,94 @@ Extract and migrate GWAS summary statistics from legacy GenomicsDB to hipFG-comp
 
 Note this is meant to be a one-off; which is why SQL queries included hard-coded primary keys
 that will not be valid for future instantiations of the database.
+
+# TODO: extract statistics correction logic into variant parsers
+# TODO: extract primary key generators
 """
 
 import argparse
 import asyncio
+from enum import Enum, auto
 import logging
 import os
 from logging import Logger
+from typing import AsyncGenerator, Optional
 
 from niagads.arg_parser.core import comma_separated_list
 from niagads.assembly.core import Human
 from niagads.database.session import DatabaseSessionManager
+from niagads.utils.list import qw
 from niagads.utils.logging import (
     LOG_FORMAT_STR,
     FunctionContextLoggerWrapper,
     async_timed,
 )
+from pydantic import BaseModel, field_validator, model_validator
 from sqlalchemy import Row, RowMapping, text
 
 LOG: Logger = FunctionContextLoggerWrapper(logger=logging.getLogger(__name__))
 QUERY_YIELD = 500000
+EXISTING_GWAS_FIELDS = qw(
+    "variant_record_primary_key neg_log10_pvalue pvalue_display frequency allele restricted_stats chromosome position"
+)
+TARGET_FIELDS = qw(
+    "chrom position variant_id ref alt pval neg_log10_pvalue OR z_score effect_size effect_size_se non_ref_af rsid QC_flags source_info"
+)
 
 
-from typing import AsyncGenerator
+class VariantType(Enum):
+    SNV = auto()
+    MNV = auto()
+    INDEL = auto()
+    SV = auto()
+
+
+class Variant(BaseModel):
+    chromosome: Human
+    position: int
+    ref: str
+    alt: str
+    test: str
+    ref_snp_id: Optional[str] = None
+    variant_type: Optional[VariantType] = None
+
+    @field_validator(variant_type, mode="after")
+    def resolve_variant_type(self):
+        if self.variant_type is not None:
+            return VariantType(self.variant_type)
+
+        l_ref = len(self.ref)
+        l_alt = len(self.alt)
+
+        if l_ref >= 50 or l_alt >= 50:
+            return VariantType.SV
+
+        if l_ref != l_alt:
+            return VariantType.INDEL
+
+        return VariantType.SNV
+
+    @classmethod
+    def from_row(cls, row):
+        chrm, position, ref, alt = row["metaseq_id"].split(":")
+
+        return cls(
+            chromosome=Human(chrm),
+            position=position,
+            ref=ref,
+            alt=alt,
+            ref_snp_id=row["ref_snp_id"],
+            test=row["allele"],
+        )
+
+    def has_rsid(self):
+        return self.ref_snp_id is not None
+
+    def test_is_alt(self):
+        return self.test == self.alt
+
+    def is_sv(self):
+        return self.variant_type == VariantType.SV
 
 
 @async_timed
@@ -34,25 +99,42 @@ async def retrieve_gwas_data(
 ) -> AsyncGenerator[RowMapping, None]:
 
     # extracting by chromosome to reduce sorting overhead
-    sql = """SELECT * FROM Results.VariantGWAS 
+    sql = f"""
+        SELECT {','.join(EXISTING_GWAS_FIELDS)}, 
+        props.details->>'metaseq_id' AS variant_id,
+        props.details->>'ref_snp_id' AS ref_snp_id 
+        FROM Results.VariantGWAS r,
+        get_variant_display_details(r.variant_record_primary_key) as props
         WHERE protocol_app_node_id = :id
         AND chromosome = :chr
         ORDER BY position ASC
     """
 
     chromosomes = Human.list()
-    for chr in chromosomes:
+    for chrm in chromosomes:
+        LOG.debug(f"Retrieving data for {dataset_id}: {chrm}")
         result = await session.execute(
             text(sql).execution_options(stream_results=True, yield_per=QUERY_YIELD),
-            {"id": dataset_id, "chr": chr},
+            {"id": dataset_id, "chr": chrm},
         )
         row: Row
         async for row in result:
             yield row._mapping  # should return a RowMapping (dict equivalent)
 
 
-def transform(data: RowMapping, file_handle, pvalue_file_handle):
+def write_association(variant: Variant, data: RowMapping, fh, pvfh):
     pass
+
+
+# TODO transformation logic
+def transform(data: RowMapping, fh, pvfh, is_lifted: bool):
+    variant = Variant.from_row(data)
+    if is_lifted and variant.is_sv():
+        return
+    if variant.has_rsid() and variant.test_is_alt():
+        write_association(variant, data, fh, pvfh)
+    if not variant.test_is_alt():
+        pass
 
 
 # plus chromosome, position
@@ -62,12 +144,13 @@ def transform(data: RowMapping, file_handle, pvalue_file_handle):
 
 async def process_dataset(dataset_id: str, session):
     LOG.info(f"Processing dataset: {dataset_id}")
-    file_prefix = os.path.join(args.output_dir, dataset_id)
+    is_lifted = "_GRCh38_" in dataset_id
+    file_prefix = os.path.join(args.output_dir, dataset_id.replace("_GRCh38_", "_"))
     file_name = f"{file_prefix}_restricted.txt"
     pvalue_file_name = f"{file_prefix}.txt"
     with open(file_name, "w") as fh, open(pvalue_file_name, "w") as pfh:
         async for row in retrieve_gwas_data(dataset_id, session):
-            transform(row, fh, pfh)
+            transform(row, fh, pfh, is_lifted=is_lifted)
 
 
 def resolve_accession_datasets(accession: str, session):
