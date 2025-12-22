d197bcc63a00da9442d22bb13a291b02fe586a15
fossilfriend <egreenfest@gmail.com>
2025-12-22 17:14:01 -0500

    handle debug level

--- components/niagads/common/core.py
+++ components/niagads/common/core.py
@@ -7,11 +7,12 @@ class ComponentBaseMixin:
     """
     Generic base class with debug, logger, and verbose members.
     """
-    logger: logging.Logger = FunctionContextAdapter(
-        logging.getLogger(__name__), {}
-    )
 
+    logger: logging.Logger = FunctionContextAdapter(logging.getLogger(__name__), {})
 
     def __init__(self, debug: bool = False, verbose: bool = False):
         self._debug: bool = debug
         self._verbose: bool = verbose
+
+        if self._debug:
+            self.logger.setLevel = "DEBUG"
