c6fa0160f6310d60f30b877ed538c36a084f0375
fossilfriend <egreenfest@gmail.com>
2026-01-17 21:11:50 -0500

    update ontology vertex; typos

--- components/niagads/genomicsdb/schema/reference/sql/ontology_graph/create_graph.sql
+++ components/niagads/genomicsdb/schema/reference/sql/ontology_graph/create_graph.sql
@@ -3,38 +3,39 @@ BEGIN
     CREATE GRAPH IF NOT EXISTS Ontology;
 
     CREATE VERTEX TYPE IF NOT EXISTS Reference.Ontology.term (
-        term_id VARCHAR(32) PRIMARY KEY,       -- e.g. GO:0006915
-        term_iri VARCHAR(150),                      -- full uri
-        term VARCHAR(512),                     -- the term
-        label VARCHAR(512),                    -- a display term for applications
+        term_id CHARACTER VARYING(32) PRIMARY KEY,       -- e.g. GO:0006915
+        term_iri CHARACTER VARYING(150),                      -- full uri
+        term CHARACTER VARYING(512),                     -- the term
+        label CHARACTER VARYING(512),                    -- a display term for applications
         definition TEXT,                       -- definitions can be long
         synonyms TEXT[],                       -- synonyms can be many / long
         is_deprecated BOOLEAN DEFAULT FALSE,
         is_placeholder BOOLEAN DEFAULT FALSE,  -- placeholder vertex
-        run_id INTEGER NOT NULL -- references admin.etltask run_id
+        run_id INTEGER NOT NULL -- references admin.etlrun run_id
     );
     
 
     CREATE EDGE TYPE IF NOT EXISTS Reference.Ontology.triple (
-        predicate VARCHAR(32) NOT NULL,         -- must point to a term_id
-        run_id INTEGER NOT NULL -- references admin.etltask run_id
+        predicate CHARACTER VARYING(32) NOT NULL,         -- must point to a term_id
+        run_id INTEGER NOT NULL -- references admin.etlrun run_id
     ) SOURCE term DESTINATION term;
 
     -- restriction blind node (BNode)
     -- TODO: Instantiate when ETL Plugin adapted to handle restrictions 
     /* CREATE VERTEX TYPE IF NOT EXISTS Reference.Ontology.restriction (
         restriction_id SERIAL PRIMARY KEY,
-        run_id INTEGER NOT NULL -- references admin.etltask run_id
+        run_id INTEGER NOT NULL -- references admin.etlrun run_id
     );*/
 
     -- link to source ontology metadata in Reference.ExternalDatabase
     CREATE VERTEX TYPE IF NOT EXISTS Reference.Ontology.ontology (
-        ontology_id INTEGER PRIMARY KEY, -- references reference.externaldatabase 
-        run_id INTEGER NOT NULL -- references admin.etltask run_id
+        external_database_id INTEGER PRIMARY KEY, -- references reference.externaldatabase 
+        ontology CHARACTER VARYING(100),
+        run_id INTEGER NOT NULL -- references admin.etlrun run_id
     );
 
     CREATE EDGE TYPE IF NOT EXISTS Reference.Ontology.defined_in (
-        run_id INTEGER NOT NULL -- references admin.etltask run_id
+        run_id INTEGER NOT NULL -- references admin.etlrun run_id
     ) SOURCE term DESTINATION ontology;
 END;
 $$ LANGUAGE plpgsql;
