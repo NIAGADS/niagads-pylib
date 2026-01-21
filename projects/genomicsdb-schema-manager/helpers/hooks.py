08013f608060ad08f58e0616450addbf63b1980b
fossilfriend <egreenfest@gmail.com>
2026-01-21 09:14:48 -0500

    add plugin registry to alembic; to do update migrationcontext to use registry

--- projects/genomicsdb-schema-manager/helpers/hooks.py
+++ projects/genomicsdb-schema-manager/helpers/hooks.py
@@ -1,4 +1,22 @@
+import importlib
+import pkgutil
+
 from sqlalchemy import Connection, Table, event
+from helpers.constants import SCHEMA_PACKAGES
+from helpers.config import Settings
+
+
+def register_schemas():
+    """
+    Dynamically import all modules in the given schema packages
+    to trigger schema registration.
+    """
+    package = importlib.import_module(Settings.from_env().SCHEMA_DEFS)
+    for _, modname, ispkg in pkgutil.walk_packages(
+        package.__path__, package.__name__ + "."
+    ):
+        if not ispkg:
+            importlib.import_module(modname)
 
 
 def register_schema_creation():
