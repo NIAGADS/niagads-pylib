7f93b05def10a05106f9f9576860753c54e9474c
fossilfriend <egreenfest@gmail.com>
2026-01-29 11:23:57 -0500

    refactor: update DeclarativeTableBase to include TransactionTableMixin

--- components/niagads/genomicsdb/schema/bases.py
+++ components/niagads/genomicsdb/schema/bases.py
@@ -1,10 +1,14 @@
-from niagads.genomicsdb.schema.mixins import HousekeepingMixin, LookupTableMixin
+from niagads.genomicsdb.schema.mixins import (
+    HousekeepingMixin,
+    LookupTableMixin,
+    TransactionTableMixin,
+)
 
 
-class DeclarativeTableBase(LookupTableMixin, HousekeepingMixin):
+class DeclarativeTableBase(LookupTableMixin, TransactionTableMixin, HousekeepingMixin):
     __abstract__ = True
 
 
 class DeclarativeMaterializedViewBase(LookupTableMixin):
-    document_primary_key = None  # so we can do primary key lookups on RAG documents
     __abstract__ = True
+    document_primary_key = None  # so we can do primary key lookups on RAG documents
