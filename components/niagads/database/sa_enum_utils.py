0ed25cccf78593ea06e4dfa9ecf233f37bc24c90
fossilfriend <egreenfest@gmail.com>
2026-01-29 17:20:50 -0500

    allow building of a database enum from multiple enums

--- components/niagads/database/sa_enum_utils.py
+++ components/niagads/database/sa_enum_utils.py
@@ -3,7 +3,9 @@ from niagads.utils.list import list_to_string
 from sqlalchemy import CheckConstraint, Column, Enum
 
 
-def enum_constraint(field_name: str, enum: CaseInsensitiveEnum):
+def enum_constraint(
+    field_name: str, enum: CaseInsensitiveEnum, use_enum_names: bool = False
+):
     """
     Returns a SQLAlchemy CheckConstraint that restricts a field to values in the given enum.
 
@@ -14,22 +16,49 @@ def enum_constraint(field_name: str, enum: CaseInsensitiveEnum):
     Returns:
         CheckConstraint: The constraint for the field.
     """
+    if not isinstance(enum, (list, tuple)):
+        enum = [enum]  # this way we can handle the use_enum_names flag only once
+
+    allowed_values = []
+    enum_cls: CaseInsensitiveEnum
+    for enum_cls in enum:
+        allowed_values.extend(enum_cls.list(return_enum_names=use_enum_names))
+    else:
+        allowed_values = enum.list()
+
     return CheckConstraint(
-        f"{field_name} in ({list_to_string(enum.list(), quote=True, delim=', ')})",
+        f"{field_name} in ({list_to_string(allowed_values, quote=True, delim=', ')})",
         name=f"check_{field_name}",
     )
 
 
-def enum_column(enum, nullable=False, index=True, native_enum=False):
+def enum_column(
+    enum: CaseInsensitiveEnum,
+    nullable=False,
+    index=True,
+    native_enum=False,
+    use_enum_names: bool = False,
+):
     """
     Returns a SQLAlchemy Column for the given enum.
 
     Args:
-        enum: The Enum class to use for the column.
+        enum: The Enum class to use for the column, or a list/tuple of Enum
+            classes. If multiple enums are provided, their values are combined
+            into a single SQLAlchemy Enum.
         nullable (bool): Whether the column is nullable.
         index (bool): Whether to create an index on the column.
 
     Returns:
         Column: The SQLAlchemy column definition.
     """
-    return Column(Enum(enum, native_enum=native_enum), nullable=nullable, index=index)
+    if not isinstance(enum, (list, tuple)):
+        enum = [enum]  # this way we can handle the use_enum_names flag only once
+
+    combined_values = []
+    enum_cls: CaseInsensitiveEnum
+    for enum_cls in enum:
+        combined_values.extend(enum_cls.list(return_enum_names=use_enum_names))
+    sa_enum = Enum(*combined_values, native_enum=native_enum)
+
+    return Column(sa_enum, nullable=nullable, index=index)
