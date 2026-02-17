b86075656f9b91d09ebaa635ede5bec9f70ae1b2
fossilfriend <egreenfest@gmail.com>
2026-02-17 11:12:44 -0500

    correct package name

--- projects/genomicsdb-schema-manager/helpers/config.py
+++ projects/genomicsdb-schema-manager/helpers/config.py
@@ -3,4 +3,4 @@ from niagads.settings.core import CustomSettings
 
 class Settings(CustomSettings):
     DATABASE_URI: str
-    SCHEMA_DEFS: str = "niagads.genomicsdb.schemas"
+    SCHEMA_DEFS: str = "niagads.genomicsdb.schema"
