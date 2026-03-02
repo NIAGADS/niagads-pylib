253d9d76e8ddbbf1dc199da713d90c4e05cbea0a
fossilfriend <egreenfest@gmail.com>
2026-03-02 16:38:08 -0500

    add commit flag handling to bootstrap database script

--- projects/genomicsdb-schema-manager/initialize-db/boostrap_db.sh
+++ projects/genomicsdb-schema-manager/initialize-db/boostrap_db.sh
@@ -1,2 +1,7 @@
-poetry run run_sql --file $PROJECT_ROOT/sql/bootstrap/create_extensions.sql
-poetry run run_sql --file $PROJECT_ROOT/sql/bootstrap/create_roles.sql
+COMMIT_FLAG=""
+if [[ "$1" == "--commit" ]]; then
+	COMMIT_FLAG="--commit"
+fi
+
+poetry run gdb_run_sql --file $PROJECT_ROOT/sql/bootstrap/create_extensions.sql $COMMIT_FLAG
+poetry run gdb_run_sql --file $PROJECT_ROOT/sql/bootstrap/create_roles.sql $COMMIT_FLAG
