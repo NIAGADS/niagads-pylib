f585bcb234ba9f912cc1ecba2df3d4c949a6c963
fossilfriend <egreenfest@gmail.com>
2026-03-02 16:38:15 -0500

    refactor role creation to use dynamic array and loop for role management

--- projects/genomicsdb-schema-manager/sql/bootstrap/create_roles.sql
+++ projects/genomicsdb-schema-manager/sql/bootstrap/create_roles.sql
@@ -1,4 +1,13 @@
-CREATE ROLE etl_runner;
-CREATE ROLE app_readonly;
-
+DO $$
+DECLARE
+  role_names text[] := ARRAY['etl_runner', 'app_readonly'];
+  role_name text;
+BEGIN
+  FOREACH role_name IN ARRAY role_names LOOP
+    IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = role_name) THEN
+      EXECUTE format('CREATE ROLE %I', role_name);
+    END IF;
+  END LOOP;
+END
+$$;
 
