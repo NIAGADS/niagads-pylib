be2adf9524164c7f4e2ea85300aa522d27f4e33a
fossilfriend <egreenfest@gmail.com>
2026-04-02 10:23:06 -0400

    cleaner recursion

--- bases/niagads/genomicsdb_etl/plugins/reference/genome_reference.py
+++ bases/niagads/genomicsdb_etl/plugins/reference/genome_reference.py
@@ -129,24 +129,6 @@ metadata_bin_gen = PluginMetadata(
 )
 
 
-BIN_INCREMENTS = [
-    -1,
-    64000000,
-    32000000,
-    16000000,
-    8000000,
-    4000000,
-    2000000,
-    1000000,
-    500000,
-    250000,
-    125000,
-    62500,
-    31250,
-    15625,
-]
-
-
 @PluginRegistry.register(metadata_bin_gen)
 class IntervalBinGenerator(AbstractBasePlugin):
     """
@@ -190,61 +172,53 @@ class IntervalBinGenerator(AbstractBasePlugin):
         bin_root: str,
         parent_bin_range: Range,
         tree_level: int,
-        sequence_length: int,
+        min_bin_width: int = 15625,
     ) -> Iterator:
-        """recursive function for generating bin index"""
-        max_tree_height = len(BIN_INCREMENTS)
-        if tree_level >= max_tree_height:
-            return
+        """Generate a chromosome-specific binary interval tree."""
 
-        bin_width = sequence_length if tree_level == 0 else BIN_INCREMENTS[tree_level]
-        bin_range = Range(
-            start=parent_bin_range.start,
-            end=parent_bin_range.start + bin_width - 1,
-            inclusive_end=True,
+        yield IntervalBin(
+            genomic_region=Range(
+                start=parent_bin_range.start,
+                end=parent_bin_range.end,
+                inclusive_end=True,
+            ),
+            bin_index=Ltree(bin_root),
+            bin_level=tree_level,
+            run_id=self.run_id,
+            chromosome=str(HumanGenome(bin_root.split(".", maxsplit=1)[0])),
         )
 
-        bin_number = 0
-        while bin_range.start <= parent_bin_range.end:
-            bin_number += 1
+        parent_bin_width = parent_bin_range.end - parent_bin_range.start + 1
+        child_bin_width = parent_bin_width // 2
 
-            bin_path = (
-                xstr(bin_root) if tree_level == 0 else f"{bin_root}.B{xstr(bin_number)}"
-            )
+        if child_bin_width < min_bin_width:
+            return
 
-            bin_range.end = min(bin_range.end, parent_bin_range.end)
+        left_child_end = parent_bin_range.start + child_bin_width - 1
+        child_level = tree_level + 1
+        child_bin_root = f"{bin_root}.L{child_level}"
 
-            # new range snapshot to avoid updates by reference
-            yield IntervalBin(
-                genomic_region=Range(
-                    start=bin_range.start,
-                    end=bin_range.end,
-                    inclusive_end=True,
-                ),
-                bin_index=Ltree(bin_path),
-                bin_level=tree_level,
-                run_id=self.run_id,
-                chromosome=str(HumanGenome(bin_path.split(".", maxsplit=1)[0])),
-            )
+        yield from self.__generate_chr_bins(
+            bin_root=f"{child_bin_root}.B1",
+            parent_bin_range=Range(
+                start=parent_bin_range.start,
+                end=left_child_end,
+                inclusive_end=True,
+            ),
+            tree_level=tree_level + 1,
+            min_bin_width=min_bin_width,
+        )
 
-            # recursively subdivide the bin to generate child levels
-            # range object is rebuilt to avoid update by reference
-            child_level = tree_level + 1
-            if child_level < max_tree_height:
-                yield from self.__generate_chr_bins(
-                    bin_root=f"{bin_path}.L{xstr(child_level)}",
-                    parent_bin_range=Range(
-                        start=bin_range.start,
-                        end=bin_range.end,
-                        inclusive_end=True,
-                    ),
-                    tree_level=child_level,
-                    sequence_length=sequence_length,
-                )
-
-            # move to the next bin on the same level
-            bin_range.start = bin_range.end + 1  # ensure bins don't overlap
-            bin_range.end = bin_range.start + bin_width - 1
+        yield from self.__generate_chr_bins(
+            bin_root=f"{child_bin_root}.B2",
+            parent_bin_range=Range(
+                start=left_child_end + 1,
+                end=parent_bin_range.end,
+                inclusive_end=True,
+            ),
+            tree_level=tree_level + 1,
+            min_bin_width=min_bin_width,
+        )
 
     def extract(self):
         for entry in self.__chr_lengths:
@@ -257,7 +231,6 @@ class IntervalBinGenerator(AbstractBasePlugin):
                     start=1, end=chromosome_length, inclusive_end=True
                 ),
                 tree_level=0,
-                sequence_length=chromosome_length,
             )
 
     def transform(self, record: IntervalBin):
