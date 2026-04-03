518e393375146b96cfc9243b3f9b149dd474ab19
fossilfriend <egreenfest@gmail.com>
2026-04-02 20:47:29 -0400

    update xref table

--- components/niagads/database/genomicsdb/schema/gene/xrefs.py
+++ components/niagads/database/genomicsdb/schema/gene/xrefs.py
@@ -1,13 +1,18 @@
+from enum import auto
+from typing import Optional
+from niagads.database.genomicsdb.schema.reference.helpers import ontology_term_fk_column
+from niagads.database.helpers import enum_column, enum_constraint
 from niagads.enums.core import CaseInsensitiveEnum
 from niagads.database.genomicsdb.schema.gene.base import GeneTableBase
 from niagads.database.genomicsdb.schema.gene.helpers import gene_fk_column
+from sqlalchemy import ForeignKey, String
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import Mapped, mapped_column
 
 from niagads.database.genomicsdb.schema.reference.mixins import ExternalDatabaseMixin
 
 
-class GeneXRefType(CaseInsensitiveEnum):
+class GeneIdentifierType(CaseInsensitiveEnum):
     ENSEMBL = "ensembl_id"
     ENTREZ = "entrez_id"
     NCBI = "entrez_id"
@@ -20,13 +25,44 @@ class GeneXRefType(CaseInsensitiveEnum):
     # TODO: orthologs?
 
 
-class GeneXRef(GeneTableBase, ExternalDatabaseMixin):
+class GeneXRefCategory(CaseInsensitiveEnum):
+    IDENTIFIER = auto()
+    LOCUS = auto()
+    PUBLICATION = auto()
+    RESOURCE_LINK = auto()
+    CLASSIFICATION = auto()
+    GROUP_MEMBERSHIP = auto()
+    ORTHOLOG = auto()
+
+
+# NOTE: not using the ExternalDatabaseMixin because there is no source_id
+class GeneXRef(GeneTableBase):
     __tablename__ = "xref"
     __table_args__ = (
-        *ExternalDatabaseMixin.__table_args__,
+        enum_constraint("xref_category", GeneXRefCategory),
         GeneTableBase.__table_args__,
     )
     _stable_id = None
     xref_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    external_database_id: Mapped[int] = mapped_column(
+        ForeignKey("reference.externaldatabase.external_database_id"),
+        nullable=False,
+        index=True,
+    )
     gene_id: Mapped[int] = gene_fk_column()
-    cross_references: Mapped[dict] = mapped_column(JSONB, nullable=True)
+    xref_category: Mapped[GeneXRefCategory] = enum_column(
+        GeneXRefCategory, index=True, nullable=False
+    )
+    xref_label: Mapped[Optional[str]] = mapped_column(
+        String(50), index=True, nullable=True
+    )
+    xref_value: Mapped[Optional[str]] = mapped_column(
+        String(250), index=True, nullable=True
+    )
+
+    ontology_term_id: Mapped[Optional[int]] = ontology_term_fk_column(
+        nullable=True, index=True
+    )
+    cross_references: Mapped[dict] = mapped_column(
+        JSONB(none_as_null=True), nullable=True
+    )
