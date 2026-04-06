ff402ea01950872e45a386ddf622b51d4265d06c
fossilfriend <egreenfest@gmail.com>
2026-04-05 22:33:20 -0400

    progress on document_mv

--- projects/genomicsdb-schema-manager/sql/schemas/gene/materialized_views/document.sql
+++ projects/genomicsdb-schema-manager/sql/schemas/gene/materialized_views/document.sql
@@ -1,39 +1,68 @@
-WITH 
-    agg_exons AS 
+WITH
+    agg_exons AS
     (   SELECT
             transcript_id,
-            jsonb_agg( jsonb_build_object( 'id', source_id, 'rank', RANK, 'location', 
+            jsonb_agg( jsonb_build_object( 'id', source_id, 'rank', RANK, 'location',
             format_genomic_location(chromosome, span, strand) ) ORDER BY RANK ) AS exons
-        FROM 
+        FROM
             Gene.Exon
-        GROUP BY 
+        GROUP BY
             transcript_id
-    ) 
+    )
     ,
-    agg_transcripts AS 
+    agg_transcripts AS
     (   SELECT
             t.gene_id,
-            jsonb_agg(jsonb_strip_nulls( jsonb_build_object( 'id', t.source_id, 'name', t.name, 'location', 
-            format_genomic_location(t.chromosome, t.span, t.strand) ) ) || 
+            jsonb_agg(jsonb_strip_nulls( jsonb_build_object( 'id', t.source_id, 'name', t.name,
+            'location', format_genomic_location(t.chromosome, t.span, t.strand) ) ) ||
             CASE
-                WHEN t.is_canonical 
+                WHEN t.is_canonical
                 THEN jsonb_build_object('is_canonical', t.is_canonical)
                 ELSE '{}'::jsonb
-            END || 
+            END ||
             CASE
-                WHEN e.exons IS NOT NULL 
+                WHEN e.exons IS NOT NULL
                 THEN jsonb_build_object('exons', e.exons)
                 ELSE '{}'::jsonb
-            END ORDER BY lower(span), upper(span)) AS transcripts
-        FROM 
+            END ORDER BY lower(span), UPPER(span)) AS transcripts
+        FROM
             Gene.Transcript t
-        LEFT JOIN 
-            agg_exons e 
-        ON 
+        LEFT JOIN
+            agg_exons e
+        ON
             e.transcript_id = t.transcript_id
-        GROUP BY gene_id
+        GROUP BY
+            gene_id
     )
-SELECT 
+    ,
+    genes AS
+    (   SELECT
+            -- index fields
+            g.gene_id,
+            g.bin_index,
+            g.chromosome,
+            g.span,
+            g.source_id,
+            -- chunked document fields
+            jsonb_build_object('gene_symbol', g.gene_symbol, 'gene_name', g.gene_name) AS
+            nomenclature,
+            jsonb_build_object('genomic_location', jsonb_build_object('chromosome', g.chromosome, 
+            'span', g.span, 'strand', g.strand), 'cytogenic_location', NULL) AS location, 
+            jsonb_build_object('curie', ot.source_id, 'term', ot.term) AS classification,
+            '{}'::JSONB AS identifers, --             '{}'::JSONB AS orthologs in with identifiers
+            '{}'::JSONB AS xrefs,
+            '{}'::JSONB AS publications,
+            '{}'::JSONB AS group_memberships -- maybe under classification? 
+            '{}'::JSONB AS annotation --pathways, GO, GVC etc
+        FROM
+            Gene.Gene              g,
+            Reference.OntologyTerm ot
+        WHERE
+            ot.ontology_term_id = g.gene_type_id
+    )
+SELECT
     *
-FROM 
-    transcripts;
+FROM
+    genes;
+    
+   
