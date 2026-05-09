3d2cafb2963c8e407bb2f4c8e7accc21ede813c6
fossilfriend <egreenfest@gmail.com>
2026-05-09 11:26:31 -0400

    add filtering

--- projects/variant-annotator/bin/s3_extract_vcf_fields.sh
+++ projects/variant-annotator/bin/s3_extract_vcf_fields.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Downloads VCF files from S3 and extracts fields 1-9 to .vcf files
-# Usage: extract_vcf_fields.sh <s3_path> <extension> [parallel_jobs]
+# Downloads VCF files from S3 and extracts fields 1-9 to .vcf files (ignoreing sample data)
+# Can also prefilter for only those variants that 'PASSED' QC
+# Usage: extract_vcf_fields.sh <s3_path> <extension> [parallel_jobs] [filter_pass]
 
 set -euo pipefail
 
 s3_path="${1%/}"
 ext="$2"
 parallel_jobs="${3:-10}"
+filter_pass="${4:-false}"
+logfile="extract_vcf_fields.log"
 
 # Extract bucket name from s3_path
 s3_bucket=$(echo "$s3_path" | sed 's|s3://\([^/]*\).*|\1|')
@@ -16,15 +19,17 @@ process_file() {
     local s3_bucket="$1"
     local file="$2"
     local ext="$3"
+    local logfile="$4"
+    local filter_pass="$5"
     local name=$(basename "$file" | sed "s/\.$ext$//")
     local tmp=$(mktemp -p .)
-    aws s3 cp "s3://$s3_bucket/$file" "$tmp"
-    zcat "$tmp" | awk '/^#CHROM/ {print; next} /^#/ {next} {print}' | cut -f1-9 > "$name"
+    aws s3 cp --no-progress "s3://$s3_bucket/$file" "$tmp"
+    pigz -dc "$tmp" | awk -v filter="$filter_pass" '/^#CHROM/ {print; next} /^#/ {next} {if (filter=="true" && $7 != "PASS") next; print}' | cut -f1-9 > "$name"
     rm "$tmp"
-    echo "✓ $name.vcf"
+    echo "$name" >> "$logfile"
 }
 
 export -f process_file
 
 files=$(aws s3 ls "$s3_path/" --recursive | awk '{print $4}' | grep "\.$ext$")
-echo "$files" | xargs -P "$parallel_jobs" -I {} bash -c "process_file $s3_bucket {} $ext"
+echo "$files" | xargs -P "$parallel_jobs" -I {} bash -c "process_file $s3_bucket {} $ext $logfile $filter_pass"
