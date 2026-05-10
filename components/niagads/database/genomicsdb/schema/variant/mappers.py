9333bb0b13ecb1d6d9d5b56363295e247f67f50f
fossilfriend <egreenfest@gmail.com>
2026-05-10 00:28:56 -0400

    variant tables

--- components/niagads/database/genomicsdb/schema/variant/mappers.py
+++ components/niagads/database/genomicsdb/schema/variant/mappers.py
@@ -1,9 +1,20 @@
-"""
-CREATE TABLE NIAGADS.MergedVariant  (
-       MERGED_VARIANT_ID	SERIAL PRIMARY KEY,
-       REF_SNP_ID		CHARACTER VARYING(25) NOT NULL,
-       MERGE_REF_SNP_ID		CHARACTER VARYING(25) NOT NULL,
-       MERGE_VARIANT_ID		NUMERIC(12),
-       MERGE_BUILD		INTEGER NOT NULL,
-       MERGE_DATE		DATE NOT NULL,
-"""
+from datetime import datetime
+from niagads.database.genomicsdb.schema.variant.base import VariantTableBase
+
+from niagads.database.helpers import datetime_column
+from sqlalchemy import INTEGER, String
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import JSONB
+
+
+class RefSnpAlias(VariantTableBase):
+    __tablename__ = "ref_snp_alias"
+    __table_args__ = (VariantTableBase.__table_args__,)
+
+    ref_snp_alias_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    ref_snp_id: Mapped[str] = mapped_column(String(25), index=True, nullable=False)
+    merged_ref_snp_id: Mapped[str] = mapped_column(
+        String(25), index=True, nullable=False
+    )
+    merge_build: Mapped[int] = mapped_column(INTEGER, nullable=False)
+    merge_date: Mapped[datetime] = datetime_column()
