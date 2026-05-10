f2172e528639dd2dd48fb41bed4effbb0d5321f4
fossilfriend <egreenfest@gmail.com>
2026-05-10 09:52:10 -0400

    handle died in fork errors due to "monoalllelic"

--- projects/variant-annotator/bin/vep.sh
+++ projects/variant-annotator/bin/vep.sh
@@ -4,6 +4,7 @@
 
 FORK=2
 
+
 # Parse named arguments: --file and --is-sv
 while [[ $# -gt 0 ]]; do
   key="$1"
@@ -34,44 +35,37 @@ WORKING_DIR=$(dirname "$FILE")
 FILE_NAME=$(basename "$FILE")
 
 INPUT_FILE=/working/${FILE_NAME}
-OUTPUT_FILE=${INPUT_FILE}.vep.json
+OUTPUT_FILE=${INPUT_FILE}.vep.json.gz
 ERROR_FILE=${INPUT_FILE}.vep.errors
 SKIP_FILE=${INPUT_FILE}.vep.skipped
 LOG_FILE=${FILE}.vep.log
 
 
-echo "WORKING_DIR=$WORKING_DIR"
-echo "INPUT_FILE=$INPUT_FILE"
-echo "OUTPUT_FILE=$OUTPUT_FILE"
-
-
-if 
-
+if bcftools reheader -h $PROJECT_DIR/niagads-pylib/projects/variant-annotator/data/vcf_header.txt $FILE | bcftools view -e 'FILTER ~ "MONOALLELIC"' | \
 docker compose --env-file $PROJECT_DIR/niagads-pylib/projects/variant-annotator/.env --file $PROJECT_DIR/niagads-pylib/projects/variant-annotator/docker-compose.yaml \
     run --rm -T -v ${WORKING_DIR}:/working vep \
   vep \
-  --input_file="${INPUT_FILE}" \
+  --input_file stdin \
   --output_file="${OUTPUT_FILE}" \
   --skipped_variants_file="${SKIP_FILE}" \
   --warning_file="${ERROR_FILE}" \
   --no_check_variants_order \
+  --no_stats \
   --format vcf \
   --force_overwrite \
+  --compress_output gzip \
   --cache \
   --verbose \
   --fork="${FORK}" \
-  --no_stats \
   --json \
   --offline \
   --everything \
-  --ga4gh_vrs \
+  --buffer_size 25000 \
   --plugin TSSDistance \
   --plugin Enformer,file=/data/enformer/enformer_grch38.vcf.gz \
   --plugin NMD \
   --plugin LoFtool,/data/LoFtool_scores.txt \
-  --plugin CADD,snv=/data/cadd/whole_genome_SNVs.tsv.gz,indels=/data/cadd/gnomad.genomes.r4.0.indel.tsv.gz >  /dev/null 2> "${LOG_FILE}"
-
-
+  --plugin CADD,snv=/data/cadd/whole_genome_SNVs.tsv.gz,indels=/data/cadd/gnomad.genomes.r4.0.indel.tsv.gz  2>&1 > "${LOG_FILE}"
 then
     echo "SUCCESS"
 else
