e3a9cb54b7b58e35b71ee664f9f84e942db88310
fossilfriend <egreenfest@gmail.com>
2026-05-11 01:03:12 -0400

    schema changes

--- components/niagads/database/genomicsdb/schema/variant/variant.py
+++ components/niagads/database/genomicsdb/schema/variant/variant.py
@@ -10,7 +10,7 @@ from niagads.database.helpers import enum_column
 from niagads.database.mixins.embeddings import EmbeddingMixin
 from niagads.database.mixins.ranges import GenomicRegionMixin
 
-from sqlalchemy import String
+from sqlalchemy import ForeignKey, Integer, String, Text
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.dialects.postgresql import JSONB
 
@@ -26,18 +26,23 @@ class Variant(VariantTableBase, IdAliasMixin, GenomicRegionMixin, EmbeddingMixin
     }
 
     variant_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    external_database_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("reference.externaldatabase.external_database_id"),
+        nullable=False,
+    )
     position: Mapped[int] = mapped_column(nullable=False)
-    ref_allele: Mapped[Optional[str]] = mapped_column(nullable=True)
-    alt_allele: Mapped[Optional[str]] = mapped_column(nullable=True)
-    display_allele: Mapped[Optional[str]] = mapped_column(nullable=True)
+    ref_allele: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
+    alt_allele: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
     length: Mapped[int] = mapped_column(nullable=False)
 
-    positional_id: Mapped[str] = mapped_column(String(150))
+    niagads_id: Mapped[str] = mapped_column(String(150), nullable=False)
+    normalized_positional_id: Mapped[str] = mapped_column(Text, nullable=True)
     ref_snp_id: Mapped[Optional[str]] = mapped_column(String(25), nullable=True)
     ga4gh_vrs: Mapped[Allele] = mapped_column(JSONB(none_as_null=True))
     hgvs: Mapped[Optional[str]] = mapped_column(nullable=True)
 
-    variant_type: Mapped[VariantClass] = enum_column(
+    variant_class: Mapped[VariantClass] = enum_column(
         VariantClass, native_enum=True, use_enum_names=True
     )
     is_adsp_variant: Mapped[Optional[bool]] = mapped_column(nullable=True)
