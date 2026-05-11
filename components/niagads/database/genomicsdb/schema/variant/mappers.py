1f3b31acebe389e1018d9578717ce096812e0337
fossilfriend <egreenfest@gmail.com>
2026-05-11 19:06:38 -0400

    add missing external_Database_id

--- components/niagads/database/genomicsdb/schema/variant/mappers.py
+++ components/niagads/database/genomicsdb/schema/variant/mappers.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from niagads.database.genomicsdb.schema.variant.base import VariantTableBase
 
 from niagads.database.helpers import datetime_column
-from sqlalchemy import INTEGER, String
+from sqlalchemy import INTEGER, ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.dialects.postgresql import JSONB
 
@@ -12,9 +12,12 @@ class RefSnpAlias(VariantTableBase):
     __table_args__ = (VariantTableBase.__table_args__,)
 
     ref_snp_alias_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    ref_snp_id: Mapped[str] = mapped_column(String(25), index=True, nullable=False)
-    merged_ref_snp_id: Mapped[str] = mapped_column(
-        String(25), index=True, nullable=False
+    external_database_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("reference.externaldatabase.external_database_id"),
+        nullable=False,
     )
+    ref_snp_id: Mapped[str] = mapped_column(String(25), index=True, nullable=False)
+    merged_into: Mapped[str] = mapped_column(String(25), index=True, nullable=False)
     merge_build: Mapped[int] = mapped_column(INTEGER, nullable=False)
     merge_date: Mapped[datetime] = datetime_column()
