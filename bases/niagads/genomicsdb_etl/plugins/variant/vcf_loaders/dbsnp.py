23392d74b766b872f444a4c0e776c9410e85f803
fossilfriend <egreenfest@gmail.com>
2026-05-11 15:23:33 -0400

    dbsnp vcf loading plugin

--- bases/niagads/genomicsdb_etl/plugins/variant/vcf_loaders/dbsnp.py
+++ bases/niagads/genomicsdb_etl/plugins/variant/vcf_loaders/dbsnp.py
@@ -1,24 +1,24 @@
 """
-VCF File Loader ETL Plugin
+DBSNP File Loader ETL Plugin
 
-Loads variants from VCF file into variant table.
-Uses cyvcf2 for fast parsing.
-Handles multiallelic variants by exploding them into separate records.
+Loads DBSNP variants from VCF file into variant table.
+- calculates GA4GH VRS
+- extracts ALFA frequencies
+**ASSUMES EMPTY TABLE**
 """
 
-from typing import Optional
+from typing import Iterator, Optional
 
 
+import cyvcf2
 from niagads.common.types import ETLOperation
-from niagads.common.variant.models.ga4gh_vrs import Allele
 from niagads.common.variant.models.record import VariantRecord
-from niagads.database.genomicsdb.schema.variant.variant import Variant
+from niagads.database.genomicsdb.schema.variant.documents import Variant
 from niagads.etl.plugins.metadata import PluginMetadata
 from niagads.etl.plugins.parameters import ResumeCheckpoint
 from niagads.etl.plugins.registry import PluginRegistry
 from niagads.etl.plugins.types import ETLLoadStrategy
-from niagads.ga4gh.annotators import GA4GHVRSService
-from niagads.ga4gh.types import VariantNomenclature
+
 from niagads.genome_reference.human import HumanGenome
 from niagads.genomicsdb_etl.plugins.variant.vcf_loaders.base import (
     BaseVCFLoader,
@@ -38,47 +38,71 @@ metadata = PluginMetadata(
 )
 
 
-@PluginRegistry.register(metadata)
-class DBSnpVCFLoader(BaseVCFLoader):
+class dbSNPRecord(VariantRecord):
+    allele_frequency: Optional[dict] = None
 
-    def transform(self, entry: VCFEntry) -> Optional[Variant]:
+
+@PluginRegistry.register(metadata)
+class dbSNPVCFLoader(BaseVCFLoader):
+
+    def __parse_allele_frequencies(self, freq_str: str, allele_index: int):
+        def get_value(values):
+            try:
+                freq_value = values[allele_index]
+                return None if freq_value == "." else float(freq_value)
+            except IndexError:
+                return None
+
+        return {
+            k: freq
+            for pair in freq_str.split("|")
+            for k, v in [pair.split(":", 1)]
+            for freq in [get_value(v.split(","))]
+            if freq is not None
+        }
+
+    def extract(self) -> Iterator[VCFEntry]:
+        """Extract variants from VCF."""
+        reader = cyvcf2.Reader(self._params.file)
+        try:
+            for entry in reader:
+                # index starts at 1 b/c ref is 0 in lists in INFO annotations
+                for allele_index, alt in enumerate(entry.ALT, start=1):
+                    vcf_entry = VCFEntry.from_cyvcf2_variant(entry, alt_allele=alt)
+                    if "FREQ" in vcf_entry.info:  # ALFA frequencies
+                        vcf_entry.info["FREQ"] = self.__parse_allele_frequencies(
+                            vcf_entry.info["FREQ"], allele_index
+                        )
+                    else:
+                        vcf_entry.info["FREQ"] = None
+                    yield vcf_entry
+
+        finally:
+            reader.close()
+
+    def transform(self, entry: VCFEntry) -> Optional[dbSNPRecord]:
         """Transform VCF variant to Variant ORM object."""
 
-        record: VariantRecord = self._generate_variant_identifier_record(
+        record: dbSNPRecord = self._generate_variant_identifier_record(
             entry, require_validation=False  # trust dbSNP
         )
 
         # TODO: extract ALFA frequencies from INFO
-        allele_freq = None
-
-        chromosome = HumanGenome(entry.chromosome)
-        bin_index = self._find_bin_index(
-            str(chromosome), record.span.start, record.span.end
-        )
-
-        return Variant(
-            chromosome=chromosome,
-            bin_index=bin_index,
-            position=record.position,
-            span=record.span,
-            length=record.length,
-            ref_allele=record.ref,
-            alt_allele=record.alt,
-            variant_class=str(record.variant_class),
-            positional_id=record.positional_id,
-            normalized_positional_id=record.normalized_positional_id,
-            ref_snp_id=record.ref_snp_id,
-            ga4gh_vrs=record.ga4gh_vrs.model_dump(),
-            is_structural_variant=record.variant_class.is_structural_variant(),
-            allele_frequency=allele_freq,
-            run_id=self.run_id,
-            external_database_release_id=self.external_database_id,
-        )
+        record.allele_frequency = None
 
     async def load(
-        self, session: AsyncSession, records: list[Variant]
+        self, session: AsyncSession, records: list[dbSNPRecord]
     ) -> Optional[ResumeCheckpoint]:
-        if not records:
-            return None
-        await Variant.submit_many(session, records)
+        variants = []
+        for record in records:
+            variant = Variant.from_variant_record(record)
+            variant.allele_frequency = record.allele_frequency
+            variant.run_id = self.run_id
+            variant.bin_index = self._find_bin_index(
+                str(record.chromosome), record.span.start, record.span.end
+            )
+            variant.external_database_id = self.external_database_id
+            variants.append(variant)
+
+        await Variant.submit_many(session, variants)
         return self.create_checkpoint(record=records[-1])
