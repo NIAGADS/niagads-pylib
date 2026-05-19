fb6c594044ddb415d566d1afe2ac2b305395d9e4
fossilfriend <egreenfest@gmail.com>
2026-05-19 08:04:58 -0400

    removed responseview

--- components/niagads/api/common/models/domain/parameters/response/content.py
+++ components/niagads/api/common/models/domain/parameters/response/content.py
@@ -1,4 +1,5 @@
 from enum import auto
+from typing import Self
 
 from niagads.api.common.models.domain.parameters.types import EnumParameter
 
@@ -7,84 +8,47 @@ class ResponseContent(EnumParameter):
     """enum for allowable response types"""
 
     FULL = auto()
-    COUNTS = auto()
-    IDS = auto()
     BRIEF = auto()
     URLS = auto()
-    # SUMMARY = auto()  - for data summaries? or separate endpoints
+    COUNTS = auto()
+    IDS = auto()
 
     @classmethod
-    def get_description(cls, inclValues=True):
-        message = "Type of information returned by the query."
-        return message + f" {super().get_description()}" if inclValues else message
+    def description(cls, include_values=True):
+        message = "Response content (full vs selected subset)"
+        return message + f"{super().description()}" if include_values else message
 
     @classmethod
-    def descriptive(cls, inclUrls=False, description=False):
+    def label(cls):
+        return "content"
+
+    @classmethod
+    def entity_record(cls, has_urls=False) -> Self:
         """return descriptive formats only (usually for metadata)"""
-        exclude = (
-            [ResponseContent.IDS, ResponseContent.COUNTS]
-            if inclUrls
-            else [ResponseContent.IDS, ResponseContent.URLS, ResponseContent.COUNTS]
-        )
-        subset = cls.exclude("descriptive_only_content", exclude)
-        if description:
-            return cls.get_description(False) + " " + subset.get_description()
-        else:
-            return subset
+        members = [ResponseContent.FULL, ResponseContent.BRIEF, ResponseContent.IDS]
+        if has_urls:
+            members.append(ResponseContent.URLS)
+        return cls.subset("record_document", members)
 
     @classmethod
-    def data(cls, description=False):
+    def feature_record(cls) -> Self:
         """return data formats only"""
-        subset = cls.exclude(
-            "data_only_content",
-            [ResponseContent.IDS, ResponseContent.URLS, ResponseContent.BRIEF],
-        )
-        if description:
-            return cls.get_description(False) + " " + subset.get_description()
-        else:
-            return subset
-
-    @classmethod
-    def full_data(cls, description=False):
-        """return full data formats only"""
-        subset = cls.exclude(
-            "full_data_only_content",
-            [ResponseContent.IDS, ResponseContent.URLS, ResponseContent.BRIEF],
-        )
-        if description:
-            return cls.get_description(False) + " " + subset.get_description()
-        else:
-            return subset
+        members = [ResponseContent.FULL, ResponseContent.COUNTS]
+        return cls.subset("data_only_content", members)
 
 
 class ResponseFormat(EnumParameter):
     """enum for allowable response / output formats"""
 
-    JSON = auto()
-    TEXT = auto()
-
-    @classmethod
-    def get_description(cls, inclValues=True):
-        message = "Response format.  If a non-text `view` is specified, the response format will default to `JSON`"
-        return message + f" {super().get_description()}" if inclValues else message
-
-
-class ResponseView(EnumParameter):
-    """enum for allowable views"""
-
-    TABLE = auto()
-    IGV_BROWSER = auto()
     DEFAULT = auto()
+    TEXT = auto()
+    TABLE = auto()  # reformat for a NIAGADS UI Table
 
     @classmethod
-    def get_description(cls, inclValues=True):
-        message = "Visual representation of the data.  Select `DEFAULT` for TEXT or JSON response."
-        return message + f" {super().get_description()}" if inclValues else message
+    def label(cls):
+        return "format"
 
     @classmethod
-    def table(cls, description=False):
-        subset = cls.exclude("table_views", [ResponseView.IGV_BROWSER])
-        if description:
-            return cls.get_description(False) + " " + subset.get_description()
-        else:
-            return subset
+    def description(cls, include_values=True):
+        message = "Response format."
+        return message + f" {super().description()}" if include_values else message
