9771c1ab00d702f31510e7db5f3082a16f28f320
fossilfriend <egreenfest@gmail.com>
2026-06-15 10:47:08 -0400

    todo note for span indexes

--- projects/genomicsdb-schema-manager/sql/schemas/variant/tables/create_variant_tuning_indexes.sql
+++ projects/genomicsdb-schema-manager/sql/schemas/variant/tables/create_variant_tuning_indexes.sql
@@ -2,3 +2,9 @@
 -- EMBEDDING INDEXES BUILD AFTER BIG LOAD, don't forget to make them partial (i.e., embedding not null)
 
 CREATE INDEX IX_VARIANT_VARIANT__EMBEDDING_RUN_ID ON Variant.Variant(EMBEDDING_RUN_ID) WHERE EMBEDDING_RUN_ID IS NOT NULL;
+
+/* TODO: gist index on span for SVs, but probably need to do same way did bin_index index
+CREATE INDEX ix_variant_variant__span_gist
+ON Variant.Variant
+USING GIST (span);
+*/
