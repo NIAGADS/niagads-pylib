78c6626aab07f63baddfe1457ae14594f88b7ade
fossilfriend <egreenfest@gmail.com>
2026-06-15 08:39:47 -0400

    fix index names

--- projects/genomicsdb-schema-manager/sql/schemas/variant/tables/create_parent_bin_index_index.sql
+++ projects/genomicsdb-schema-manager/sql/schemas/variant/tables/create_parent_bin_index_index.sql
@@ -9,78 +9,78 @@ CREATE INDEX IF NOT EXISTS ix_variant_variant__bin_index
 ON ONLY variant.variant USING GIST (bin_index);
 
 -- Attach each existing child index to the parent index.
--- These names must match the indexes created on the partitions.
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_1__bin_index;
+ATTACH PARTITION variant.ix_variant_chr1__bin_index;
+
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_2__bin_index;
+ATTACH PARTITION variant.ix_variant_chr2__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_3__bin_index;
+ATTACH PARTITION variant.ix_variant_chr3__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_4__bin_index;
+ATTACH PARTITION variant.ix_variant_chr4__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_5__bin_index;
+ATTACH PARTITION variant.ix_variant_chr5__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_6__bin_index;
+ATTACH PARTITION variant.ix_variant_chr6__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_7__bin_index;
+ATTACH PARTITION variant.ix_variant_chr7__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_8__bin_index;
+ATTACH PARTITION variant.ix_variant_chr8__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_9__bin_index;
+ATTACH PARTITION variant.ix_variant_chr9__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_10__bin_index;
+ATTACH PARTITION variant.ix_variant_chr10__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_11__bin_index;
+ATTACH PARTITION variant.ix_variant_chr11__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_12__bin_index;
+ATTACH PARTITION variant.ix_variant_chr12__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_13__bin_index;
+ATTACH PARTITION variant.ix_variant_chr13__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_14__bin_index;
+ATTACH PARTITION variant.ix_variant_chr14__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_15__bin_index;
+ATTACH PARTITION variant.ix_variant_chr15__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_16__bin_index;
+ATTACH PARTITION variant.ix_variant_chr16__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_17__bin_index;
+ATTACH PARTITION variant.ix_variant_chr17__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_18__bin_index;
+ATTACH PARTITION variant.ix_variant_chr18__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_19__bin_index;
+ATTACH PARTITION variant.ix_variant_chr19__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_20__bin_index;
+ATTACH PARTITION variant.ix_variant_chr20__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_21__bin_index;
+ATTACH PARTITION variant.ix_variant_chr21__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_22__bin_index;
+ATTACH PARTITION variant.ix_variant_chr22__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_x__bin_index;
+ATTACH PARTITION variant.ix_variant_chrx__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_y__bin_index;
+ATTACH PARTITION variant.ix_variant_chry__bin_index;
 
 ALTER INDEX variant.ix_variant_variant__bin_index
-ATTACH PARTITION variant.ix_variant_mt__bin_index;
+ATTACH PARTITION variant.ix_variant_chrm__bin_index;
