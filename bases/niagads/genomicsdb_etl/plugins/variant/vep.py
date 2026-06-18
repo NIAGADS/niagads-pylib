b975037ef94ec649f3b6f5f1d2647452000db9cb
fossilfriend <egreenfest@gmail.com>
2026-06-18 12:27:58 -0400

    summary model

--- bases/niagads/genomicsdb_etl/plugins/variant/vep.py
+++ bases/niagads/genomicsdb_etl/plugins/variant/vep.py
@@ -2,32 +2,65 @@ import json
 from typing import Optional
 from niagads.common.types import ETLOperation
 from niagads.database.genomicsdb.schema.variant.documents import Variant
+from niagads.etl.plugins.base import AbstractBasePlugin
 from niagads.etl.plugins.metadata import PluginMetadata
-from niagads.etl.plugins.parameters import BasePluginParams, PathValidatorMixin
+from niagads.etl.plugins.parameters import BasePluginParams, EmbeddingParameterMixin, PathValidatorMixin
+from niagads.etl.plugins.registry import PluginRegistry
+from pydantic import Field, field_validator
 from niagads.etl.plugins.types import ETLLoadStrategy
-from pydantic import Field
+
+from niagads.nlp.llm_types import LLM, NLPModelType
+
+
 # need to filter consequences for variant allele != variant alt allele
 # ditto for allele frequencies
-
 # only is len(colocated_variant >  1)
-
 # af in colocated-variants
 
 
-
-class VEPJSONLoaderParams(BasePluginParams, PathValidatorMixin):
+class VEPAnnotationLoaderParams(BasePluginParams, PathValidatorMixin, EmbeddingParameterMixin):
     file: str = Field(description="Path to VEP JSON file")
-    is_adsp: Optional[bool] = Field(default=False, description="Insert novel variants only; skip flagging existing variants as is_adsp_variant")
-
+    summarization_model: Optional[LLM] = Field(
+        default=LLM.BART_LARGE_CNN, description="LLM model for generating textual summaries of the VEP annotation")
     validate_file_exists = PathValidatorMixin.validator("file")
+
+    @field_validator("prompt_model")
+    @classmethod
+    def validate_embedding_model(cls, v: LLM) -> LLM:
+        """Validate that embedding_model is in allowed embedding models list."""
+        LLM.validate(v, NLPModelType.SUMMARIZATION)
+        return LLM(v)
+
 
 metadata = PluginMetadata(
     version="1.0",
-    description="Load variants from VCF file into variant table",
+    description=f"Update existing {Variant.table_name()} records with VEP annotation." "Includes translating to human readable text and calculating embeddings",
     affected_tables=[Variant],
+
     load_strategy=ETLLoadStrategy.CHUNKED,
-    operation=ETLOperation.LOAD,
+    operation=ETLOperation.UPDATE,
     is_large_dataset=True,
-    parameter_model=VEPJSONLoaderParams,
-    can_resume=False, # TODO: implement resume
+    parameter_model=VEPAnnotationLoaderParams,
+    can_resume=True,
 )
+
+
+@PluginRegistry.register(metadata=metadata)
+class VEPAnnotationLoader(AbstractBasePlugin):
+    _params: VEPAnnotationLoaderParams
+
+    def __init__(self, params, name=None, log_path=None, debug=False, verbose=False):
+        super().__init__(params, name, log_path, debug, verbose)
+
+    async def on_run_start(self, session):
+        # initialize embedding models
+        ...
+
+    def extract(self):
+        return super().extract()
+
+    async def transform(self, data):
+        return await super().transform(data)
+
+    async def load(self, session, transformed):
+        return await super().load(session, transformed)
