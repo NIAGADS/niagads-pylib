c3e286a8f64408434a9e2976bf20d8a9f3a77516
fossilfriend <egreenfest@gmail.com>
2026-07-01 10:35:45 -0400

    allow sniffing freedom to detect delimiter, avoid alphnums and fallback to \t , without limiting to that

--- components/niagads/csv_parser/core.py
+++ components/niagads/csv_parser/core.py
@@ -8,6 +8,8 @@ from pandas import read_csv, DataFrame
 from niagads.utils.dict import convert_str2numeric_values
 from niagads.utils.pandas import strip_df
 
+LIST_DELIMITERS = {";", "|", "/", "="}
+
 
 class CSVFileParser(ComponentBaseMixin):
     """
@@ -97,23 +99,34 @@ class CSVFileParser(ComponentBaseMixin):
         """
         'sniff' out / infer the delimitier
         """
+        invalid_delimiter = None
         try:
             if self.__sep is not None:
                 return self.__sep
             else:
                 with open(self.__file, "r", encoding="utf-8", errors="ignore") as fh:
-                    dialect: Dialect = Sniffer().sniff(
-                        fh.read(bytes), delimiters=[",", "\t", ";", "|"]
+                    sample = fh.read(bytes)
+
+                delimiter: str = Sniffer().sniff(sample).delimiter
+                if delimiter in LIST_DELIMITERS or delimiter.isalnum():
+                    invalid_delimiter = delimiter
+                    delimiter = (
+                        Sniffer().sniff(sample, delimiters=[",", "\t"]).delimiter
                     )
-                    fh.seek(0)
-                    return dialect.delimiter
+
+                return delimiter
+
         except CSVError as err:
             if bytes < 4096:  # try a larger section of the file
                 return self.sniff(bytes=4096)
+            if invalid_delimiter is not None:
+                raise FileFormatError(
+                    f"Invalid delimiter detected: {invalid_delimiter!r}."
+                )
             raise FileFormatError(
                 "Unable to determine file delimiter."
                 "File may have inconsistent numbers of columns, sparse data, "
-                "or use inconsistent or non-standard delimiters. "
+                "or use inconsistent delimiters."
             ) from err
 
     def to_pandas_df(self, transpose=False, **kwargs) -> DataFrame:
