a76739b1902ef2c0ca628883c9eb6cc11eed5e72
fossilfriend <egreenfest@gmail.com>
2026-08-24 09:50:10 -0400

    documentation

--- components/niagads/rdf_parsers/mesh.py
+++ components/niagads/rdf_parsers/mesh.py
@@ -1,25 +1,29 @@
-"""
-Basic parsing of MeSH N-Triples to extract Topical Descriptor and Concept level terms
-for dataset annotation.  To leverage MeSH for additional search
-it probably would be best to store as graph w/combination indexing table that includes
-the tree numbers (represented as an LTRee, if sticking to postgres).
-See https://www.nlm.nih.gov/mesh/intro_retrieval.html
+"""Parse MeSH N-Triples into descriptors, concepts, and terms.
 
-i.e., now just retrieving topical descriptors, preferred terms, and preferred concepts
-so can translate into ontology terms and synonyms
+The parser extracts topical descriptors, preferred terms, preferred concepts,
+and synonyms for dataset annotation. See the MeSH retrieval documentation for
+background: https://www.nlm.nih.gov/mesh/intro_retrieval.html.
 """
 
 from typing import Generator, Iterator, Optional
 
 from niagads.common.models.base import CustomBaseModel
 from niagads.rdf_parsers.rdf import NTriplesParser
-from pydantic import Field
 from rdflib import RDF, RDFS, URIRef
 
 MeSHV = "http://id.nlm.nih.gov/mesh/vocab#"
 
 
 class MeSHBaseElement(CustomBaseModel):
+    """Common fields shared by MeSH descriptors, concepts, and terms.
+
+    Attributes:
+        iri: The element's IRI in the MeSH vocabulary.
+        id: The MeSH identifier.
+        label: The element's display label.
+        is_active: Whether the element is active in MeSH.
+    """
+
     iri: str
     id: str
     label: str
@@ -27,23 +31,47 @@ class MeSHBaseElement(CustomBaseModel):
 
 
 class MeSHTerm(MeSHBaseElement):
+    """A MeSH term and its optional abbreviation and alternate labels.
+
+    Attributes:
+        abbreviation: The term's abbreviation, when one is defined.
+        alt_labels: Alternate labels associated with the term.
+    """
+
     abbreviation: Optional[str] = None
     alt_labels: Optional[list[str]] = None
 
 
 class MeSHConcept(MeSHBaseElement):
+    """A MeSH concept with its preferred term and related terms.
+
+    Attributes:
+        preferred_term: The concept's preferred term.
+        scope_note: A note describing the scope of the concept.
+        terms: Terms associated with the concept.
+    """
+
     preferred_term: MeSHTerm
     scope_note: Optional[str] = None
     terms: Optional[list[MeSHTerm]] = None
 
 
 class MeSHDescriptor(MeSHBaseElement):
+    """A MeSH topical descriptor and its preferred concept.
+
+    Attributes:
+        is_active: Whether the descriptor is active in MeSH.
+        preferred_concept: The descriptor's preferred concept.
+    """
+
     is_active: bool
     preferred_concept: MeSHConcept
     # concepts: Optional[list[MeSHConcept]] = None
 
 
 class MeSHParser(NTriplesParser):
+    """Extract MeSH entities from an N-Triples RDF graph."""
+
     def __init__(
         self,
         file: str,
@@ -51,6 +79,14 @@ class MeSHParser(NTriplesParser):
         debug: bool = False,
         verbose: bool = False,
     ):
+        """Initialize a parser for a MeSH N-Triples file.
+
+        Args:
+            file: Path to the MeSH N-Triples file.
+            logger: Optional logger used for parser messages.
+            debug: If True, enable debug mode.
+            verbose: If True, enable verbose logging.
+        """
 
         super().__init__(
             file=file,
@@ -60,10 +96,15 @@ class MeSHParser(NTriplesParser):
             verbose=verbose,
         )
 
-    # self._namespace.TopicalDescriptor
     def extract_topical_descriptors(
         self,
     ) -> Iterator[MeSHDescriptor]:
+        """Yield topical descriptors from the MeSH graph.
+
+        Returns:
+            Iterator[MeSHDescriptor]: Topical descriptors, including each
+                descriptor's preferred concept.
+        """
         for descriptor in self._graph.subjects(
             RDF.type, self._namespace.TopicalDescriptor
         ):
@@ -88,9 +129,28 @@ class MeSHParser(NTriplesParser):
             )
 
     def _get_linked_terms(self, object: URIRef) -> Generator:
+        """Return terms linked to a MeSH concept.
+
+        Args:
+            object: IRI of the concept whose linked terms should be returned.
+
+        Returns:
+            Generator: Term IRIs linked through the MeSH ``terms`` predicate.
+        """
         return self._graph.objects(subject=object, predicate=self._namespace.terms)
 
     def extract_term(self, term: URIRef):
+        """Extract a MeSH term from the graph.
+
+        Preferred labels replace the regular label when both are present. The
+        replaced label is retained as an alternate label when necessary.
+
+        Args:
+            term: IRI of the MeSH term to extract.
+
+        Returns:
+            MeSHTerm: A populated MeSH term model.
+        """
         preferred_label = self.get_value(
             subject=term, predicate=self._namespace.prefLabel, as_string=True
         )
@@ -123,6 +183,15 @@ class MeSHParser(NTriplesParser):
         )
 
     def extract_concept(self, concept: URIRef):
+        """Extract a MeSH concept and its associated terms.
+
+        Args:
+            concept: IRI of the MeSH concept to extract.
+
+        Returns:
+            MeSHConcept: A populated MeSH concept model containing the
+                preferred term and all terms linked to the concept.
+        """
         preferred_term = self.get_value(
             subject=concept, predicate=self._namespace.preferredTerm
         )
@@ -143,5 +212,10 @@ class MeSHParser(NTriplesParser):
         )
 
     def extract_concepts(self) -> Iterator[MeSHConcept]:
+        """Yield all MeSH concepts found in the graph.
+
+        Returns:
+            Iterator[MeSHConcept]: Populated MeSH concept models.
+        """
         for concept in self._graph.subjects(RDF.type, self._namespace.Concept):
             yield self.extract_concept(concept)
